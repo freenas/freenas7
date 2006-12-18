12e24327178be95148f3b3f153e22b1dcd3c62b8
votdev <votdev@localhost>
2006-12-18 22:25:31 +0000

    Fix bug in clog build script.

--- misc/software/clog/build.sh
+++ misc/software/clog/build.sh
@@ -1,52 +1,26 @@
 #!/usr/bin/env bash
 
-URL_CLOG="http://www.freenas.org/downloads/clog-1.0.1.tar.gz"
-URL_SYSLOGD="http://www.freenas.org/downloads/syslogd_clog-current.tgz"
-
 build_clog() {
-	cd /usr/src/usr.bin/
+	cd /usr/src/usr.sbin
 
-  clog_tarball=$(urlbasename $URL_CLOG)
-  syslogd_tarball=$(urlbasename $URL_SYSLOGD)
+	tar -zxvf $SVNDIR/misc/software/clog/syslogd_clog-current.tgz
+  tar -zxvf $SVNDIR/misc/software/clog/clog-1.0.1.tar.gz
 
-  if [ ! -f "$clog_tarball" ]; then
-		fetch $URL_CLOG
-		if [ 1 == $? ]; then
-      echo "==> Failed to fetch $clog_tarball."
-      return 1
-    fi
-	fi
-	if [ ! -f "$syslogd_tarball" ]; then
-    fetch $URL_SYSLOGD
-    if [ 1 == $? ]; then
-      echo "==> Failed to fetch $syslogd_tarball."
-      return 1
-    fi
-	fi
-
-  tar zxvf $clog_tarball
-  tar zxvf $syslogd_tarball
-
-  cd syslogd
+  cd /usr/src/usr.sbin/syslogd
   make
 
-  cd ../clog
+  cd /usr/src/usr.sbin/clog
   gcc clog.c -o clog
 
 	return 0
 }
 
 install_clog() {
-	clog_tarball=$(urlbasename $URL_CLOG)
-	syslogd_tarball=$(urlbasename $URL_SYSLOGD)
+	cd /usr/src/usr.sbin/syslogd
+  install -vs syslogd $FREENAS/usr/sbin
 
-  cd /usr/src/usr.bin/
-
-	cd syslogd	
-  install -vs syslogd $FREENAS/usr/sbin/
-	
-	cd ../clog
-  install -vs clog $FREENAS/usr/sbin/	
+	cd /usr/src/usr.sbin/clog
+  install -vs clog $FREENAS/usr/sbin	
 
 	return 0
 }
