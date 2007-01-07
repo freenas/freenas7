4414c71d632e55045ab77cd90fdd1b3334592968
votdev <votdev@localhost>
2007-01-07 14:11:36 +0000

    Fix warning message (WARNING: /usr/local/etc/ssh/moduli does not exist, using fixed modulus) in sshd log.

--- misc/software/openssh-portable/build.sh
+++ misc/software/openssh-portable/build.sh
@@ -4,8 +4,8 @@ build_openssh-portable() {
 	cd /usr/ports/security/openssh-portable/
 
 	# Copy options file
-	mkdir /var/db/ports/openssh/
-	cp -v -p $SVNDIR/misc/software/openssh-portable/files/options /var/db/ports/openssh/
+	mkdir -pv /var/db/ports/openssh
+	cp -pv $SVNDIR/misc/software/openssh-portable/files/options /var/db/ports/openssh
 
 	make clean
 	make
@@ -16,8 +16,13 @@ build_openssh-portable() {
 install_openssh-portable() {
 	cd /usr/ports/security/openssh-portable/
 
-	install -vs work/openssh-*/sshd $FREENAS/usr/sbin/
-	install -vs work/openssh-*/sftp-server $FREENAS/usr/libexec/
+	install -vs work/openssh-*/sshd $FREENAS/usr/sbin
+	install -vs work/openssh-*/sftp-server $FREENAS/usr/libexec
+
+	# Create link to moduli file to prevent log entry:
+	# WARNING: /usr/local/etc/ssh/moduli does not exist, using fixed modulus
+	mkdir -pv $FREENAS/usr/local/etc/ssh
+	ln -sv $FREENAS/etc/ssh/moduli $FREENAS/usr/local/etc/ssh/moduli
 
 	return 0
 }
