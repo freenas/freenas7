70f6f83ac2253b7216853ce2ed72b70b4aca3957
votdev <votdev@localhost>
2007-01-15 13:27:16 +0000

    Fix misc bugs in translation files and disks_mount_fsck.php.

--- www/disks_mount_fsck.php
+++ www/disks_mount_fsck.php
@@ -36,7 +36,7 @@
 */
 require("guiconfig.inc");
 
-$pgtitle = array(_DISKSPHP_NAME,_DISKSMOUNTTOOLS_NAMEDESC);
+$pgtitle = array(_DISKSPHP_NAME,_DISKSMOUNTFSCK_NAMEDESC);
 
 if (!is_array($config['mounts']['mount']))
 	$config['mounts']['mount'] = array();
@@ -67,9 +67,9 @@ if (!isset($do_fsck)) {
   <tr>
     <td class="tabnavtbl">
       <ul id="tabnav">
- <li class="tabinact"><a href="disks_mount.php"><?=_DISKSMOUNTPHP_MANAGE;?></a></li>
+				<li class="tabinact"><a href="disks_mount.php"><?=_DISKSMOUNTPHP_MANAGE;?></a></li>
         <li class="tabinact"><a href="disks_mount_tools.php"><?=_DISKSMOUNTPHP_TOOLS;?></a></li>
- <li class="tabact"><?=_DISKSMOUNTPHP_FSCK;?></a></li>
+				<li class="tabact"><?=_DISKSMOUNTPHP_FSCK;?></a></li>
       </ul>
     </td>
   </tr>
@@ -83,7 +83,7 @@ if (!isset($do_fsck)) {
             <td class="vtable">
               <select name="disk" class="formfld" id="disk" onchange="disk_change()">
                 <?php foreach ($a_mount as $mount): ?>
-			<?php if (strcmp($mount['fstype'],"cd9660") == 0) continue; ?>
+								<?php if (strcmp($mount['fstype'],"cd9660") == 0) continue; ?>
                 <option value="<?=$mount['fullname'];?>"<?php if ($mount['fullname'] == $disk) echo "selected";?>>
                 <?php echo htmlspecialchars($mount['sharename'] . ": " .$mount['fullname']);?>
                 <?php endforeach; ?>
@@ -95,8 +95,8 @@ if (!isset($do_fsck)) {
             <td width="22%" valign="top" class="vncell"></td>
             <td width="78%" class="vtable"> 
               <input name="umount" type="checkbox" id="umount" value="yes" <?php if ($umount) echo "checked"; ?>>
-              <strong><?=_DISKSMOUNTTOOLS_UMOUNT;?></strong><span class="vexpl"><br>
-              <?=_DISKSMOUNTTOOLS_UMOUNTTEXT;?></span>
+              <strong><?=_DISKSMOUNTFSCK_UMOUNT;?></strong><span class="vexpl"><br>
+              <?=_DISKSMOUNTFSCK_UMOUNTTEXT;?></span>
             </td>
           </tr>
   				<tr>
@@ -107,46 +107,41 @@ if (!isset($do_fsck)) {
   				</tr>
   				<tr>
     				<td valign="top" colspan="2">
-    				<?php if($do_fsck)
-    				{
-    				  echo("<strong>" . _DISKSMOUNTTOOLS_CMDINFO . "</strong><br>");
-    					echo('<pre>');
-    					ob_end_flush();
+						<?php if($do_fsck) {
+							echo("<strong>" . _DISKSMOUNTFSCK_CMDINFO . "</strong><br>");
+							echo('<pre>');
+							ob_end_flush();
 
-              
-                  /* Get the id of the disk. */
-		              $id = array_search_ex($disk, $a_mount, "fullname");
-		              /* Get the filesystem type of the disk. */ 
-		              $type = $a_mount[$id]['fstype'];
-                  /* Check if disk is mounted. */
+							/* Get the id of the disk. */
+							$id = array_search_ex($disk, $a_mount, "fullname");
+							/* Get the filesystem type of the disk. */ 
+							$type = $a_mount[$id]['fstype'];
+							/* Check if disk is mounted. */
+							$ismounted = disks_check_mount_fullname($disk);
+							/* Umount disk if necessary. */
+							if($umount && $ismounted) {
+								echo("<strong class='red'>" . _NOTE . ":</strong> " . _DISKSMOUNTFSCK_MOUNTNOTE . "<br><br>");
+								disks_umount_fullname($disk);
+							}
 
-		$ismounted = disks_check_mount_fullname($disk);
-
-                  /* Umount disk if necessary. */
-		              if($umount && $ismounted) {
-		                echo("<strong class='red'>" . _NOTE . ":</strong> " . _DISKSMOUNTTOOLS_MOUNTNOTE . "<br><br>");
-		                disks_umount_fullname($disk);
-                  }
-
-                  switch($type)
-        					{
-                    case "":
-          					case "ufs":
-                      system("/sbin/fsck_ufs -y -f " . escapeshellarg($disk));
-          						break;
-          					case "msdosfs":
-                      system("/sbin/fsck_msdosfs -y -f " . escapeshellarg($disk));
-          						break;
-        					}
-
-                  /* Mount disk if necessary. */
-        					if($umount && $ismounted) {
-		                disks_mount_fullname($disk);
-
-              }
-    					echo('</pre>');
-    				}
-    				?>
+							switch($type)
+							{
+								case "":
+								case "ufs":
+									system("/sbin/fsck_ufs -y -f " . escapeshellarg($disk));
+									break;
+								case "msdosfs":
+									system("/sbin/fsck_msdosfs -y -f " . escapeshellarg($disk));
+									break;
+							}
+							
+							/* Mount disk if necessary. */
+							if($umount && $ismounted) {
+								disks_mount_fullname($disk);
+							}
+							echo('</pre>');
+						}
+						?>
     				</td>
   				</tr>
 			 </table>
