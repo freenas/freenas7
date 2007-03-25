54efb57550aca0cc0718e8849d64d2263195961c
votdev <votdev@localhost>
2007-03-25 12:37:34 +0000

    Fix bug in WebGUI. Remember last selected disk on disk format page.

--- www/disks_init.php
+++ www/disks_init.php
@@ -250,7 +250,7 @@ function fstype_change() {
 					<?php if (strcmp($diskv['size'],"NA") == 0) continue; ?>
 					<?php if (strcmp($diskv['fstype'],"geli") == 0) continue; ?>
 					<?php if (disks_geli_check($diskv['fullname'])) continue; ?>
-          <option value="<?=$diskv['fullname'];?>" <?php if ($diskv['name'] == $disk) echo "selected";?>><?php echo htmlspecialchars($diskv['name'] . ": " .$diskv['size'] . " (" . $diskv['desc'] . ")");?></option>
+          <option value="<?=$diskv['fullname'];?>" <?php if ($diskv['fullname'] == $disk) echo "selected";?>><?php echo htmlspecialchars($diskv['name'] . ": " .$diskv['size'] . " (" . $diskv['desc'] . ")");?></option>
           <?php endforeach; ?>
         </select>
       </td>
