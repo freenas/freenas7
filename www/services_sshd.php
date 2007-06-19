e8bb96dd7dcf2f86df26314d316a1762418c92f6
votdev <votdev@localhost>
2007-06-19 07:04:42 +0000

    Add 'sshd' rc init script.

--- www/services_sshd.php
+++ www/services_sshd.php
@@ -84,7 +84,7 @@ if ($_POST)
 		$retval = 0;
 		if (!file_exists($d_sysrebootreqd_path)) {
 			config_lock();
-			$retval |= services_sshd_configure();
+			$retval |= rc_update_service("sshd");
 			$retval |= rc_update_service("mdnsresponder");
 			config_unlock();
 		}
