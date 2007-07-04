b176cea73e58eb0df05539a57bbe7495dbecca21
votdev <votdev@localhost>
2007-07-04 10:08:28 +0000

    - Fix bug in msntp runmsntp.sh script. If there were multiple timeservers configured only the first is used. - Improve XML configuration query.

--- build/ports/msntp/files/runmsntp.sh
+++ build/ports/msntp/files/runmsntp.sh
@@ -2,10 +2,15 @@
 # write our PID to file
 echo $$ > $1
 
+_pidfile=$2
+_timeupdateinterval=$3
+shift 3
+_timeservers=$@
+
 # execute msntp in endless loop; restart if it
 # exits (wait 1 second to avoid restarting too fast in case
 # the network is not yet setup)
 while true; do
-	/usr/local/bin/msntp -r -P no -l $2 -x $3 $4
+	/usr/local/bin/msntp -r -P no -l ${_pidfile} -x ${_timeupdateinterval} ${_timeservers}
 	sleep 1
 done
