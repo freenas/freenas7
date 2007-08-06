a0d6da57fa39e7bc4f70cf74bdc5f8709606719a
votdev <votdev@localhost>
2007-08-06 13:39:44 +0000

    Begin to implement StatusReport class that can be used my Status Report email and Status.php webgui page. It is planned to support plain text and HTML.

--- etc/mail/sendreport.php
+++ etc/mail/sendreport.php
@@ -6,68 +6,34 @@
 require_once("config.inc");
 require_once("util.inc");
 require_once("disks.inc");
+require_once("report.inc");
 require_once("phpmailer/class.phpmailer.php");
 
-// Define a command, with a title, to be executed later.
-function defCmdT($title, $command) {
-	global $commands;
-	$commands[] = array($title, $command);
-}
-
-// Execute a command, with a title, and generate command results output.
-function doCmdT(&$body, $title, $command) {
-	// Set title.
-	if (!empty($title)) {
-		$nWidth = strlen($title) + 1;
-		$body .= sprintf("%s:\n%'-{$nWidth}s\n",$title,"");
-	}
-
-	// Execute command and retrieve output.
-	if (!empty($command)) {
-		exec($command . " 2>&1", $execOutput, $execStatus);
-		for ($i = 0; isset($execOutput[$i]); $i++) {
-			if ($i > 0) {
-				$body .= "\n";
-			}
-			$body .= $execOutput[$i];
-		}
-		$body .= "\n\n";
-	}
-}
-
-// Execute all of the commands.
-function create_email_body(&$body) {
-	global $commands;
-	for ($i = 0; isset($commands[$i]); $i++) {
-		doCmdT($body, $commands[$i][0], $commands[$i][1]);
-	}
-}
-
-// Define commands added in status email.
-defCmdT("Version","cat /etc/prd.version");
-defCmdT("Platform","cat /etc/platform");
-defCmdT("System uptime","uptime");
-defCmdT("Interfaces","/sbin/ifconfig -a");
-defCmdT("Routing tables","netstat -nr");
-defCmdT("Processes","ps xauww");
-defCmdT("Swap usege","/usr/sbin/swapinfo");
-defCmdT("Sensors","/usr/local/bin/chm -I -d 0");
-defCmdT("ATA disk","/sbin/atacontrol list");
-defCmdT("SCSI disk","/sbin/camcontrol devlist");
-defCmdT("SMART","");
+$statusreport = new StatusReport();
+$statusreport->AddCommand("Version","cat /etc/prd.version");
+$statusreport->AddCommand("Platform","cat /etc/platform");
+$statusreport->AddCommand("System uptime","uptime");
+$statusreport->AddCommand("Interfaces","/sbin/ifconfig -a");
+$statusreport->AddCommand("Routing tables","netstat -nr");
+$statusreport->AddCommand("Processes","ps xauww");
+$statusreport->AddCommand("Swap usege","/usr/sbin/swapinfo");
+$statusreport->AddCommand("Sensors","/usr/local/bin/chm -I -d 0");
+$statusreport->AddCommand("ATA disk","/sbin/atacontrol list");
+$statusreport->AddCommand("SCSI disk","/sbin/camcontrol devlist");
+$statusreport->AddTitle("S.M.A.R.T.");
 $disklist=get_physical_disks_list();
 foreach ($disklist as $disknamek => $disknamev) {
-	defCmdT("","/usr/local/sbin/smartctl -a /dev/{$disknamek}");
+	$statusreport->AddCommand("/usr/local/sbin/smartctl -a /dev/{$disknamek}");
 }
-defCmdT("Geom Concat","/sbin/gconcat list");
-defCmdT("Geom Stripe","/sbin/gstripe list");
-defCmdT("Geom Mirror","/sbin/gmirror list");
-defCmdT("Geom RAID5","/sbin/graid5 list");
-defCmdT("Geom Vinum","/sbin/gvinum list");
-defCmdT("Mount point","/sbin/mount");
-defCmdT("Free Disk Space","/bin/df -h");
-defCmdT("Encrypted disks","/sbin/geli list");
-defCmdT("Last 200 system log entries","/usr/sbin/clog /var/log/system.log 2>&1 | tail -n 200");
+$statusreport->AddCommand("Geom Concat","/sbin/gconcat list");
+$statusreport->AddCommand("Geom Stripe","/sbin/gstripe list");
+$statusreport->AddCommand("Geom Mirror","/sbin/gmirror list");
+$statusreport->AddCommand("Geom RAID5","/sbin/graid5 list");
+$statusreport->AddCommand("Geom Vinum","/sbin/gvinum list");
+$statusreport->AddCommand("Mount point","/sbin/mount");
+$statusreport->AddCommand("Free Disk Space","/bin/df -h");
+$statusreport->AddCommand("Encrypted disks","/sbin/geli list");
+$statusreport->AddCommand("Last 200 system log entries","/usr/sbin/clog /var/log/system.log 2>&1 | tail -n 200");
 
 $mail = new PHPMailer();
 $mail->IsSMTP();
@@ -89,8 +55,8 @@ if (isset($config['statusreport']['auth'])) {
 	$mail->Password = base64_decode($config['statusreport']['password']);
 }
 
-// Create email body.
-create_email_body($mail->Body);
+// Create report.
+$mail->Body = $statusreport->Generate();
 
 // Send email and log result.
 if(!$mail->Send()) {
