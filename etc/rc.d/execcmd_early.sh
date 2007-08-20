d1184159aa4dd9f795f933442ee80ee972b49d7e
votdev <votdev@localhost>
2007-08-20 12:24:20 +0000

    Fix bug in xml processing. Strings containing the characters <>" produces errors in the corresponding scripts. This is because the special characters were not unescaped: < <=> &lt; > <=> &gt; " <=> &quot; ...

--- etc/rc.d/execcmd_early.sh
+++ etc/rc.d/execcmd_early.sh
@@ -6,12 +6,13 @@
 # REQUIRE: system_init rcconf
 # BEFORE: CONFIG
 
+. /etc/rc.subr
 . /etc/configxml.subr
 
 _index=`/usr/local/bin/xml sel -t -v "count(//system/earlyshellcmd)" ${configxml_file}`
 while [ ${_index} -gt 0 ]
 do
-	_cmd=`/usr/local/bin/xml sel -t -v "//system/earlyshellcmd[${_index}]" ${configxml_file}`
+	_cmd=`configxml_get "//system/earlyshellcmd[${_index}]"`
 	eval ${_cmd}
 	_index=$(( ${_index} - 1 ))
 done
--- etc/rc.d/execcmd_late.sh
+++ etc/rc.d/execcmd_late.sh
@@ -5,12 +5,13 @@
 # PROVIDE: execcmd_late
 # REQUIRE: LOGIN
 
+. /etc/rc.subr
 . /etc/configxml.subr
 
 _index=`/usr/local/bin/xml sel -t -v "count(//system/shellcmd)" ${configxml_file}`
 while [ ${_index} -gt 0 ]
 do
-	_cmd=`/usr/local/bin/xml sel -t -v "//system/shellcmd[${_index}]" ${configxml_file}`
+	_cmd=`configxml_get "//system/shellcmd[${_index}]"`
 	eval ${_cmd}
 	_index=$(( ${_index} - 1 ))
 done
