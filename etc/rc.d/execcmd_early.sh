ac40fcfa36b7df908610b8ebac3dd4be8ff2e613
votdev <votdev@localhost>
2007-08-20 12:25:22 +0000

    Fix bug in xml processing. Strings containing the characters <>" produces errors in the corresponding scripts. This is because the special characters were not unescaped: < <=> &lt; > <=> &gt; " <=> &quot; ...

--- etc/rc.d/execcmd_early.sh
+++ etc/rc.d/execcmd_early.sh
@@ -12,7 +12,7 @@
 _index=`/usr/local/bin/xml sel -t -v "count(//system/earlyshellcmd)" ${configxml_file}`
 while [ ${_index} -gt 0 ]
 do
-	_cmd=`/usr/local/bin/xml sel -t -v "//system/earlyshellcmd[${_index}]" ${configxml_file}`
+	_cmd=`configxml_get "//system/earlyshellcmd[${_index}]"`
 	eval ${_cmd}
 	_index=$(( ${_index} - 1 ))
 done
--- etc/rc.d/execcmd_late.sh
+++ etc/rc.d/execcmd_late.sh
@@ -11,7 +11,7 @@
 _index=`/usr/local/bin/xml sel -t -v "count(//system/shellcmd)" ${configxml_file}`
 while [ ${_index} -gt 0 ]
 do
-	_cmd=`/usr/local/bin/xml sel -t -v "//system/shellcmd[${_index}]" ${configxml_file}`
+	_cmd=`configxml_get "//system/shellcmd[${_index}]"`
 	eval ${_cmd}
 	_index=$(( ${_index} - 1 ))
 done
