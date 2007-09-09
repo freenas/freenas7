5e27a852b81920e2de603da079c6558b834eb967
votdev <votdev@localhost>
2007-09-09 16:52:00 +0000

    Add script to extract gettext strings from source.

--- build/freenas-create-pot.sh
+++ build/freenas-create-pot.sh
@@ -5,7 +5,7 @@
 # Global variables
 FREENAS_ROOTDIR="/usr/local/freenas"
 FREENAS_SVNDIR="$FREENAS_ROOTDIR/svn"
-FREENAS_PRODUCTNAME=$(cat $FREENAS_SVNDIR/etc/prd.name)
+FREENAS_PRODUCTNAME=$(cat $FREENAS_SVNDIR/etc/prd.name | tr '[:upper:]' '[:lower:]')
 
 PARAMETERS="--output-dir=${FREENAS_SVNDIR}/locale --output=${FREENAS_PRODUCTNAME}.pot \
 --force-po --indent --no-location --no-wrap --sort-output --omit-header"
