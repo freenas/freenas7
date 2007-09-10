0ba0dd6e33d7b75ba3ff473a4970f57b819e5c05
votdev <votdev@localhost>
2007-09-10 09:24:38 +0000

    Fix typo in disks_crypt_edit.php WebGUI.

--- www/disks_crypt_edit.php
+++ www/disks_crypt_edit.php
@@ -321,7 +321,7 @@ if (!isset($do_crypt)) {
 			    <tr>
 			      <td width="22%" valign="top">&nbsp;</td>
 			      <td width="78%"><span class="vexpl"><span class="red"><strong><?=gettext("Warning"); ?>:<br>
-			        </strong></span><?=htmlentities(gettext("This will erase ALL data on your disk!<br>Using Data integrity will reduce size of available storage and also reduce speed."));?></span>
+			        </strong></span><?=gettext("This will erase ALL data on your disk!<br>Using Data integrity will reduce size of available storage and also reduce speed.");?></span>
 			      </td>
 			    </tr>
 			  </table>
