55c007c8fe9de0e01ad7524258c1ce57655c89fa
votdev <votdev@localhost>
2007-09-10 05:30:13 +0000

    Update translation template and freenas.pot creation script.

--- build/freenas-create-pot.sh
+++ build/freenas-create-pot.sh
@@ -5,13 +5,30 @@
 # Global variables
 FREENAS_ROOTDIR="/usr/local/freenas"
 FREENAS_SVNDIR="$FREENAS_ROOTDIR/svn"
-FREENAS_PRODUCTNAME=$(cat $FREENAS_SVNDIR/etc/prd.name | tr '[:upper:]' '[:lower:]')
+FREENAS_PRODUCTNAME=$(cat ${FREENAS_SVNDIR}/etc/prd.name)
 
-PARAMETERS="--output-dir=${FREENAS_SVNDIR}/locale --output=${FREENAS_PRODUCTNAME}.pot \
---force-po --indent --no-location --no-wrap --sort-output --omit-header"
+OUTPUT="$(echo ${FREENAS_PRODUCTNAME} | tr '[:upper:]' '[:lower:]').pot"
+OUTPUTDIR="${FREENAS_SVNDIR}/locale"
+PARAMETERS="--output-dir=${OUTPUTDIR} --output=${OUTPUT} \
+--force-po --no-location --no-wrap --sort-output --omit-header"
 
 cd ${FREENAS_SVNDIR}/www
 xgettext ${PARAMETERS} *.*
 
 cd ${FREENAS_SVNDIR}/www
 xgettext ${PARAMETERS} --join-existing *.*
+
+echo "msgid \"\"
+msgstr \"\"
+\"Project-Id-Version: ${FREENAS_PRODUCTNAME}\\n\"
+\"POT-Creation-Date: \\n\"
+\"PO-Revision-Date: $(date "+%Y-%m-%d %H:%M")+0000\\n\"
+\"Last-Translator: \\n\"
+\"Language-Team: \\n\"
+\"MIME-Version: 1.0\\n\"
+\"Content-Type: text/plain; charset=iso-8859-1\\n\"
+\"Content-Transfer-Encoding: 8bit\\n\"
+" >${OUTPUTDIR}/${OUTPUT}.tmp
+
+cat ${OUTPUTDIR}/${OUTPUT} >>${OUTPUTDIR}/${OUTPUT}.tmp
+mv -f ${OUTPUTDIR}/${OUTPUT}.tmp ${OUTPUTDIR}/${OUTPUT}
