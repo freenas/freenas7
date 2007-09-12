e10f37861eaac5bb588cc370a62e824e3e40a1bf
votdev <votdev@localhost>
2007-09-12 05:44:08 +0000

    Display 'MTU' on LAN configuration page.

--- www/interfaces_lan.php
+++ www/interfaces_lan.php
@@ -1,23 +1,23 @@
 #!/usr/local/bin/php
-<?php 
+<?php
 /*
 	interfaces_lan.php
 	part of FreeNAS (http://www.freenas.org)
 	Based on m0n0wall (http://m0n0.ch/wall)
-	
+
 	Copyright (C) 2005-2007 Olivier Cochard-Labbe <olivier@freenas.org>.
 	All rights reserved.
-	
+
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
-	
+
 	1. Redistributions of source code must retain the above copyright notice,
 	   this list of conditions and the following disclaimer.
-	
+
 	2. Redistributions in binary form must reproduce the above copyright
 	   notice, this list of conditions and the following disclaimer in the
 	   documentation and/or other materials provided with the distribution.
-	
+
 	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -57,7 +57,14 @@ $pconfig['gateway'] = get_defaultgateway();
 $pconfig['ipv6gateway'] = get_ipv6defaultgateway();
 $pconfig['dhcphostname'] = $config['system']['hostname'] . "." . $config['system']['domain'];
 $pconfig['dhcpclientidentifier'] = get_macaddr($lancfg['if']);
-$pconfig['mtu'] = $lancfg['mtu'];
+
+if (empty($lancfg['mtu'])) {
+	$ifinfo = get_interface_info($lancfg['if']);
+	$pconfig['mtu'] = $ifinfo['mtu'];
+} else {
+	$pconfig['mtu'] = $lancfg['mtu'];
+}
+
 $pconfig['media'] = $lancfg['media'];
 $pconfig['mediaopt'] = $lancfg['mediaopt'];
 $pconfig['polling'] = isset($lancfg['polling']);
@@ -79,43 +86,28 @@ if ($_POST) {
 	if ($_POST['type'] == "Static")   {
 		$reqdfields = explode(" ", "ipaddr subnet");
 		$reqdfieldsn = array(gettext("IP address"),gettext("Subnet bit count"));
-	
+
 		do_input_validation($_POST, $reqdfields, $reqdfieldsn, &$input_errors);
 
-		/*$reqdfields = explode(" ", "ipaddr gateway");
-		$reqdfieldsn = array(gettext("IP address"),gettext("Gateway"));
-		$reqdfieldst = explode(" ", "ipaddr ipaddr"); */
-		
 		if (($_POST['ipaddr'] && !is_ipv4addr($_POST['ipaddr'])))
 			$input_errors[] = gettext("A valid IPv4 address must be specified.");
 		if ($_POST['subnet'] && !filter_var($_POST['subnet'], FILTER_VALIDATE_INT, array('options' => array('min_range' => 1, 'max_range' => 32))))
 			$input_errors[] = gettext("A valid network bit count (1-32) must be specified.");
 	}
-	
+
 	if ($_POST['ipv6type'] == "Static")   {
 		$reqdfields = array_merge($reqdfields,explode(" ", "ipv6addr ipv6subnet"));
 		$reqdfieldsn = array_merge($reqdfieldsn,array(gettext("IPv6 address"),gettext("Prefix")));
-	
+
 		do_input_validation($_POST, $reqdfields, $reqdfieldsn, &$input_errors);
 
-		/*$reqdfields = array_merge($reqdfields,explode(" ", "ipv6addr ipv6gateway"));
-		$reqdfieldsn = array_merge($reqdfieldsn,array(gettext("IPv6 address"),gettext("Gateway")));
-		$reqdfieldst = array_merge($reqdfieldst,explode(" ", "ipv6addr ipv6addr")); */
-		
 		if (($_POST['ipv6addr'] && !is_ipv6addr($_POST['ipv6addr'])))
 			$input_errors[] = gettext("A valid IPv6 address must be specified.");
 		if ($_POST['ipv6subnet'] && !filter_var($_POST['ipv6subnet'], FILTER_VALIDATE_INT, array('options' => array('min_range' => 1, 'max_range' => 128))))
 			$input_errors[] = gettext("A valid prefix (1-128) must be specified.");
 		if (($_POST['ipv6gatewayr'] && !is_ipv6addr($_POST['ipv6gateway'])))
 			$input_errors[] = gettext("A valid IPv6 Gateway address must be specified.");
-		
 	}
-	
-	/*$reqdfields = array_merge($reqdfields, explode(" ", "mtu"));
-	$reqdfieldsn = array_merge($reqdfieldsn, array(gettext("MTU")));
-	$reqdfieldst = array_merge($reqdfieldst, explode(" ", "mtu"));
-
-	do_input_validation_type($_POST, $reqdfields, $reqdfieldsn, $reqdfieldst, &$input_errors); */
 
 	/* Wireless interface? */
 	if (isset($lancfg['wireless'])) {
@@ -133,7 +125,7 @@ if ($_POST) {
 		} else if (strcmp($_POST['type'],"DHCP") == 0) {
 			$config['interfaces']['lan']['ipaddr'] = "dhcp";
 		}
-		
+
 		if(strcmp($_POST['ipv6type'],"Static") == 0) {
 			$config['interfaces']['lan']['ipv6addr'] = $_POST['ipv6addr'];
 			$config['interfaces']['lan']['ipv6subnet'] = $_POST['ipv6subnet'];
@@ -162,7 +154,7 @@ if ($_POST) {
 					unlink($d_landirty_path);
 				}
 			}
-			// Reload page to update disabled controls, otherwise they're empty. 
+			// Reload page to update disabled controls, otherwise they're empty.
 			header("Location: interfaces_lan.php");
 		} else {
 			touch($d_landirty_path);
@@ -170,7 +162,7 @@ if ($_POST) {
 	}
 }
 ?>
-<?php include("fbegin.inc"); ?>
+<?php include("fbegin.inc");?>
 <script language="JavaScript">
 <!--
 /* Calculate default IPv4 netmask bits for network's class. */
@@ -181,7 +173,7 @@ function calc_netmask_bits(ipaddr) {
             return "";
         if (adr[0] == 0 && adr[1] == 0 && adr[2] == 0 && adr[3] == 0)
             return "";
-		
+
 		if (adr[0] <= 127)
 			return "8";
 		else if (adr[0] <= 191)
@@ -256,26 +248,26 @@ function ipv6_type_change() {
   <input name="apply" type="submit" class="formbtn" id="apply" value="<?=gettext("Apply changes");?>"></p>
 <?php endif; ?>
   <table width="100%" border="0" cellpadding="6" cellspacing="0">
-    <tr> 
+    <tr>
       <td colspan="2" valign="top" class="listtopic"><?=gettext("IPv4 Configuration"); ?></td>
     </tr>
-    <tr> 
+    <tr>
     	<td width="22%" valign="top" class="vncellreq"><?=gettext("Type"); ?></td>
       <td width="78%" class="vtable">
   			<select name="type" class="formfld" id="type" onchange="type_change()">
           <?php $opts = split(" ", "Static DHCP"); foreach ($opts as $opt): ?>
-          <option <?php if ($opt == $pconfig['type']) echo "selected";?>> 
+          <option <?php if ($opt == $pconfig['type']) echo "selected";?>>
             <?=htmlspecialchars($opt);?>
           </option>
           <?php endforeach; ?>
         </select>
       </td>
     </tr>
-    <tr> 
+    <tr>
       <td width="22%" valign="top" class="vncellreq"><?=gettext("IP address"); ?></td>
-      <td width="78%" class="vtable"> 
+      <td width="78%" class="vtable">
         <input name="ipaddr" type="text" class="formfld" id="ipaddr" size="20" value="<?=htmlspecialchars($pconfig['ipaddr']);?>">
-        / 
+        /
         <select name="subnet" class="formfld" id="subnet">
           <?php for ($i = 31; $i > 0; $i--): ?>
           <option value="<?=$i;?>" <?php if ($i == $pconfig['subnet']) echo "selected"; ?>>
@@ -286,7 +278,7 @@ function ipv6_type_change() {
         <img name="calcnetmaskbits" src="calc.gif" title="<?=gettext("Calculate netmask bits");?>" width="16" height="17" align="top" border="0" onclick="change_netmask_bits()" style="cursor:pointer">
       </td>
     </tr>
-     <tr> 
+     <tr>
       <td valign="top" class="vncell"><?=gettext("IPv4 Gateway"); ?></td>
       <td class="vtable">
         <input name="gateway" type="text" class="formfld" id="gateway" size="20" value="<?=htmlspecialchars($pconfig['gateway']);?>">
@@ -309,30 +301,30 @@ function ipv6_type_change() {
     <tr>
 			<td colspan="2" class="list" height="12"></td>
 		</tr>
-		<tr> 
+		<tr>
       <td colspan="2" valign="top" class="listtopic"><?=gettext("IPv6 Configuration"); ?></td>
     </tr>
-    <tr> 
+    <tr>
     	<td width="22%" valign="top" class="vncellreq"><?=gettext("Type"); ?></td>
       <td width="78%" class="vtable">
   			<select name="ipv6type" class="formfld" id="ipv6type" onchange="ipv6_type_change()">
           <?php $opts = split(" ", "Static Auto"); foreach ($opts as $opt): ?>
-          <option <?php if ($opt == $pconfig['ipv6type']) echo "selected";?>> 
+          <option <?php if ($opt == $pconfig['ipv6type']) echo "selected";?>>
             <?=htmlspecialchars($opt);?>
           </option>
           <?php endforeach; ?>
         </select>
       </td>
     </tr>
-    <tr> 
+    <tr>
       <td width="22%" valign="top" class="vncellreq"><?=gettext("IPv6 address"); ?></td>
-      <td width="78%" class="vtable"> 
+      <td width="78%" class="vtable">
         <input name="ipv6addr" type="text" class="formfld" id="ipv6addr" size="30" value="<?=htmlspecialchars($pconfig['ipv6addr']);?>">
 		 /
 		 <input name="ipv6subnet" type="text" class="formfld" id="ipv6subnet" size="2" value="<?=htmlspecialchars($pconfig['ipv6subnet']);?>">
       </td>
     </tr>
-		<tr> 
+		<tr>
       <td valign="top" class="vncell"><?=gettext("IPv6 Gateway"); ?></td>
       <td class="vtable">
         <input name="ipv6gateway" type="text" class="formfld" id="ipv6gateway" size="20" value="<?=htmlspecialchars($pconfig['ipv6gateway']);?>">
@@ -341,46 +333,46 @@ function ipv6_type_change() {
 		<tr>
 			<td colspan="2" class="list" height="12"></td>
 		</tr>
-		<tr> 
+		<tr>
       <td colspan="2" valign="top" class="listtopic"><?=gettext("Global Configuration"); ?></td>
     </tr>
-    <tr> 
+    <tr>
       <td valign="top" class="vncell"><?=gettext("MTU"); ?></td>
       <td class="vtable">
         <input name="mtu" type="text" class="formfld" id="mtu" size="20" value="<?=htmlspecialchars($pconfig['mtu']);?>">&nbsp;<br>
         <span class="vexpl"><?=gettext("Standard MTU is 1500, use 9000 for jumbo frame."); ?></span>
       </td>
     </tr>
-    <tr> 
+    <tr>
       <td width="22%" valign="top" class="vncell"><?=gettext("Device polling"); ?></td>
-      <td width="78%" class="vtable"> 
+      <td width="78%" class="vtable">
         <input name="polling" type="checkbox" id="polling" value="yes" <?php if ($pconfig['polling']) echo "checked"; ?>>
         <?=gettext("Enable device polling"); ?><br>
         <span class="vexpl"><?=gettext("Device polling is a technique that lets the system periodically poll network devices for new data instead of relying on interrupts. This can reduce CPU load and therefore increase throughput, at the expense of a slightly higher forwarding delay (the devices are polled 1000 times per second). Not all NICs support polling."); ?></span>
       </td>
     </tr>
-    <tr> 
+    <tr>
       <td width="22%" valign="top" class="vncell"><?=gettext("Speed"); ?></td>
       <td width="78%" class="vtable">
         <select name="media" class="formfld" id="media">
           <?php $types = explode(",", "autoselect,10baseT/UTP,100baseTX,1000baseTX,1000baseSX");
           $vals = explode(" ", "autoselect 10baseT/UTP 100baseTX 1000baseTX 1000baseSX");
           $j = 0; for ($j = 0; $j < count($vals); $j++): ?>
-          <option value="<?=$vals[$j];?>" <?php if ($vals[$j] == $pconfig['media']) echo "selected";?>> 
+          <option value="<?=$vals[$j];?>" <?php if ($vals[$j] == $pconfig['media']) echo "selected";?>>
           <?=htmlspecialchars($types[$j]);?>
           </option>
           <?php endfor; ?>
         </select>
       </td>
 		</tr>
-		<tr> 
+		<tr>
       <td width="22%" valign="top" class="vncell"><?=gettext("Duplex"); ?></td>
       <td width="78%" class="vtable">
         <select name="mediaopt" class="formfld" id="mediaopt">
           <?php $types = explode(",", "half-duplex,full-duplex");
           $vals = explode(" ", "half-duplex full-duplex");
           $j = 0; for ($j = 0; $j < count($vals); $j++): ?>
-          <option value="<?=$vals[$j];?>" <?php if ($vals[$j] == $pconfig['mediaopt']) echo "selected";?>> 
+          <option value="<?=$vals[$j];?>" <?php if ($vals[$j] == $pconfig['mediaopt']) echo "selected";?>>
           <?=htmlspecialchars($types[$j]);?>
           </option>
           <?php endfor; ?>
@@ -392,13 +384,13 @@ function ipv6_type_change() {
 		if (isset($lancfg['wireless']))
 			wireless_config_print();
 		?>
-    <tr> 
+    <tr>
       <td width="22%" valign="top">&nbsp;</td>
-      <td width="78%"> 
-        <input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save");?>"> 
+      <td width="78%">
+        <input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save");?>">
       </td>
     </tr>
-    <tr> 
+    <tr>
       <td width="22%" valign="top">&nbsp;</td>
       <td width="78%"><span class="vexpl"><span class="red"><strong><?=gettext("Warning"); ?>:<br>
 				</strong></span><?php echo sprintf(gettext("After you click &quot;Save&quot;, you may also have to do one or more of the following steps before you can access %s again: <ul><li>change the IP address of your computer</li><li>access the webGUI with the new IP address</li></ul>"), get_product_name());?></span>
@@ -412,4 +404,4 @@ type_change();
 ipv6_type_change();
 //-->
 </script>
-<?php include("fend.inc"); ?>
+<?php include("fend.inc");?>
