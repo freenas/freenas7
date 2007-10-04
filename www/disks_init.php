4a82579d5488a845412139ccaf25d2c8dbac31cd
votdev <votdev@localhost>
2007-10-04 12:22:37 +0000

    Set attribute 'fstype' for geli encrypted disk.

--- www/disks_init.php
+++ www/disks_init.php
@@ -253,8 +253,10 @@ if ($_POST) {
 				/* Set new filesystem type. */
  				$a_gvinum[$id]['fstype'] = $type;
 				$NotFound = 0;
+			} else {
+				$id = array_search_ex($disk, $a_geli, "fullname");
 			}
-			
+
 			/* geli */
 			if (($id !== false) && $NotFound) {
 				/* Set new filesystem type. */
