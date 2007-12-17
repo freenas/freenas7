ccaf0509e07a9071de92a60c5a81365f5637be62
votdev <votdev@localhost>
2007-12-17 13:21:22 +0000

    Upgrade netbsd-iscsi (iscsi-target) to 20071209.

---
+++ build/ports/iscsi-target/files/patch-src_md5hl.c
@@ -0,0 +1,12 @@
+--- md5hl.c.orig	Sun Dec  9 09:21:06 2007
++++ md5hl.c	Mon Dec 17 13:14:30 2007
+@@ -5,9 +5,7 @@
+  * Public domain.
+  */
+ 
+-#ifdef HAVE_CONFIG_H
+ #include <config.h>
+-#endif
+ 
+ #define	MDALGORITHM	iSCSI_MD5
+ 
