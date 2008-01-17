68f748aea528420a160c2cfa19539871ea3b174f
votdev <votdev@localhost>
2008-01-17 09:05:41 +0000

    Set primary group to 'guest' when creating a new user.

--- www/access_users_edit.php
+++ www/access_users_edit.php
@@ -58,7 +58,7 @@ if (isset($id) && $a_user[$id]) {
 	$pconfig['fullshell'] = isset($a_user[$id]['fullshell']);
 	$pconfig['homedir'] = $a_user[$id]['homedir'];
 } else {
-	$pconfig['primarygroup'] = $a_group['ftp'];
+	$pconfig['primarygroup'] = $a_group['guest'];
 }
 
 if ($_POST) {
