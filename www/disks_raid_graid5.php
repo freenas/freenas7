b6734fa0db211002548f526e6512a3379b63fd04
votdev <votdev@localhost>
2008-01-29 15:17:27 +0000

    Respect customization.

--- www/disks_raid_graid5.php
+++ www/disks_raid_graid5.php
@@ -150,9 +150,8 @@ function is_modified($name) {
 					</tr>
         </table>
       </form>
-			<p><span class="vexpl"><span class="red"><strong><?=gettext("Note");?>:</strong></span><br><?php echo sprintf( gettext("Optional configuration step: Configuring a virtual RAID disk using your <a href='%s'>previously configured disk</a>.<br>Wait for the '%s' status before format and mount it!"), "disks_manage.php", "COMPLETE");?></span></p>
-			<br>
-			<p><span class="vexpl"><span class="red"><strong><?=gettext("Info");?>:</strong></span><br><?=gettext("FreeNAS use Geom gRaid5 to create RAID5 arrays.");?>
+			<p><span class="vexpl"><span class="red"><strong><?=gettext("Note");?>:</strong></span><br><?php echo sprintf( gettext("Optional configuration step: Configuring a virtual RAID disk using your <a href='%s'>previously configured disk</a>.<br>Wait for the '%s' status before format and mount it!"), "disks_manage.php", "COMPLETE");?></span></p><br/>
+			<p><span class="vexpl"><span class="red"><strong><?=gettext("Info");?>:</strong></span><br><?=sprintf(gettext("%s uses %s to create %s arrays."), get_product_name(), "GEOM gRaid5", "RAID5");?>
 			</td>
 	</tr>
 </table>
