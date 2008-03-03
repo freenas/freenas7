c6d2eb4b136e7c47f0b4d357ce07ca6a85f7d991
votdev <votdev@localhost>
2008-03-03 09:29:58 +0000

    Do not use rounded corners if browser is IE (to prevent rendering errors in tabs).

--- www/niftycube/niftylayout.js
+++ www/niftycube/niftylayout.js
@@ -1,9 +1,14 @@
 window.onload=function(){
-Nifty("td.tabcont");
-Nifty("div#errorbox");
-Nifty("div#infobox");
-Nifty("li.tabact","transparent top");
-Nifty("li.tabinact","transparent top");
-Nifty("td.listtopic","small");
-Nifty("td.optsect_t","small");
+	Nifty("td.tabcont");
+	Nifty("div#errorbox");
+	Nifty("div#infobox");
+	Nifty("td.listtopic","small");
+	Nifty("td.optsect_t","small");
+	// Use rounded corners only if browser is not IE (to prevent
+	// rendering errors in tabs).
+	var agt = navigator.userAgent.toLowerCase();
+	if (!((agt.indexOf("msie") != -1) && (agt.indexOf("opera") == -1))) {
+		Nifty("li.tabact","transparent top");
+		Nifty("li.tabinact","transparent top");
+	}
 }
