6c8e396d667c3f0e89bf305c8241ea19d9218158
votdev <votdev@localhost>
2008-03-03 07:54:28 +0000

    Modify niftycubes.

--- www/niftycube/niftylayout.js
+++ www/niftycube/niftylayout.js
@@ -2,8 +2,8 @@ window.onload=function(){
 Nifty("td.tabcont");
 Nifty("div#errorbox");
 Nifty("div#infobox");
-Nifty("li.tabact","tl tr");
-Nifty("li.tabinact","tl tr");
+Nifty("li.tabact","transparent top");
+Nifty("li.tabinact","transparent top");
 Nifty("td.listtopic","small");
 Nifty("td.optsect_t","small");
 }
