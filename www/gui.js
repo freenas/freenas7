c63842f20ba39dbb89774ecd1fac06868afcc1fd
votdev <votdev@localhost>
2008-03-09 10:21:22 +0000

    Refactor javascript code.

--- www/gui.js
+++ www/gui.js
@@ -1,21 +1,22 @@
-function showElementById(id,state) {
-	element = document.getElementById(id);
-	if ('show' == state) {
-		element.style.display = '';
-	}
-	if ('hide' == state) {
-		element.style.display = 'none';
+function showElementById(id, state) {
+	var element = document.getElementById(id);
+	switch (state) {
+		case "show":
+			element.style.display = "";
+			break;
+		case "hide":
+			element.style.display = "none";
 	}
 }
 
 function showhideMenu(tspan, tri) {
-	tspanel = document.getElementById(tspan);
-	triel = document.getElementById(tri);
-	if (tspanel.style.display == 'none') {
-		tspanel.style.display = '';
+	var tspanel = document.getElementById(tspan);
+	var triel = document.getElementById(tri);
+	if (tspanel.style.display == "none") {
+		tspanel.style.display = "";
 		triel.src = "/tri_o.gif";
 	} else {
-		tspanel.style.display = 'none';
+		tspanel.style.display = "none";
 		triel.src = "/tri_c.gif";
 	}
 }
