1fe49f46dc645afa522a3e294712956a2038dd99
votdev <votdev@localhost>
2008-03-09 08:48:49 +0000

    - Use SAJAX to update index.php WebGUI every 5 seconds. - Misc code changes.

--- www/index.php
+++ www/index.php
@@ -33,11 +33,41 @@
   POSSIBILITY OF SUCH DAMAGE.
 */
 require("guiconfig.inc");
+require("sajax/sajax.php");
 
 $pgtitle = array(get_product_name()." webGUI");
 $pgtitle_omit = true;
+
+function update_controls() {
+	$stats['uptime'] = system_get_uptime();
+	$stats['date'] = date("D M j G:i:s T Y");
+
+	// Get RAM usage.
+	$raminfo = system_get_ram_info();
+	$percentage = round(($raminfo['used'] * 100) / $raminfo['total'], 0);
+	$stats['memusage'] = $percentage;
+	$stats['memusagetxt'] = sprintf(gettext("%d%% of %dMB"), $percentage, round($raminfo['physical'] / 1024 / 1024));
+
+	// Get load average.
+	exec("uptime", $result);
+	$stats['loadaverage'] = substr(strrchr($result[0], "load averages:"), 15);
+
+	$stats['cpuusage'] = system_get_cpu_usage();
+
+	$stats = join("|", $stats);
+
+	return $stats;
+}
+
+sajax_init();
+sajax_export("update_controls");
+sajax_handle_client_request();
 ?>
 <?php include("fbegin.inc");?>
+<script>
+<?php sajax_show_javascript();?>
+</script>
+<script type="text/javascript" src="javascript/index.js"></script>
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
   <tr align="center" valign="top">
     <td height="10" colspan="2">&nbsp;</td>
@@ -86,46 +116,61 @@ $pgtitle_omit = true;
   <tr>
     <td width="25%" class="vncellt"><?=gettext("Date");?></td>
     <td width="75%" class="listr">
-      <?php exec("/bin/date", $date); echo htmlspecialchars($date[0]); ?>
+      <?php exec("/bin/date", $date);?>
+      <input style="padding: 0; border: 0;" size="30" name="date" id="date" value="<?=htmlspecialchars($date[0]);?>"/>
     </td>
   </tr>
   <tr>
     <td width="25%" class="vncellt"><?=gettext("Uptime");?></td>
     <td width="75%" class="listr">
-			<?=htmlspecialchars(system_get_uptime());?>
+			<?php $uptime = system_get_uptime();?>
+			<input style="padding: 0; border: 0;" size="30" name="uptime" id="uptime" value="<?=htmlspecialchars($uptime);?>"/>
     </td>
   </tr>
-  <?php if ($config['lastchange']): ?>
+  <?php if ($config['lastchange']):?>
     <tr>
       <td width="25%" class="vncellt"><?=gettext("Last config change");?></td>
       <td width="75%" class="listr">
-        <?=htmlspecialchars(date("D M j G:i:s T Y", $config['lastchange']));?>
+				<input style="padding: 0; border: 0;" size="30" name="lastchange" id="lastchange" value="<?=htmlspecialchars(date("D M j G:i:s T Y", $config['lastchange']));?>"/>
       </td>
     </tr>
-  <?php endif; ?>
+  <?php endif;?>
+	<tr>
+		<td width="25%" class="vncellt">CPU usage</td>
+		<td width="75%" class="listr">
+			<?php
+			$percentage = 0;
+			echo "<img src='bar_left.gif' height='15' width='4' border='0' align='absmiddle'>";
+			echo "<img src='bar_blue.gif' name='cpuusageu' id='cpuusageu' height='15' width='" . $percentage . "' border='0' align='absmiddle'>";
+			echo "<img src='bar_gray.gif' name='cpuusagef' id='cpuusagef' height='15' width='" . (100 - $percentage) . "' border='0' align='absmiddle'>";
+			echo "<img src='bar_right.gif' height='15' width='5' border='0' align='absmiddle'> ";
+			?>
+			<input style="padding: 0; border: 0;" size="30" name="cpuusage" id="cpuusage" value="<?=gettext("Updating in 5 seconds.");?>"/>
+		</td>
+	</tr>
   <tr>
     <td width="25%" class="vncellt"><?=gettext("Memory usage");?></td>
     <td width="75%" class="listr">
-      <?php
-      	/* Get RAM information */
-				$raminfo = system_get_ram_usage();
-				/* Calculate memory use percentage. */
-        $memUsage = round(($raminfo['used'] * 100) / $raminfo['total'], 0);
-
-        echo "<img src='bar_left.gif' height='15' width='4' border='0' align='absmiddle'>";
-        echo "<img src='bar_blue.gif' height='15' width='" . $memUsage . "' border='0' align='absmiddle'>";
-        echo "<img src='bar_gray.gif' height='15' width='" . (100 - $memUsage) . "' border='0' align='absmiddle'>";
-        echo "<img src='bar_right.gif' height='15' width='5' border='0' align='absmiddle'> ";
-        echo sprintf(gettext("%d%% of %dMB"), $memUsage, round($raminfo['physical'] / 1024 / 1024));
-      ?>
+			<?php
+			$raminfo = system_get_ram_info();
+			$percentage = round(($raminfo['used'] * 100) / $raminfo['total'], 0);
+			echo "<img src='bar_left.gif' height='15' width='4' border='0' align='absmiddle'>";
+			echo "<img src='bar_blue.gif' name='memusageu' id='memusageu' height='15' width='" . $percentage . "' border='0' align='absmiddle'>";
+			echo "<img src='bar_gray.gif' name='memusagef' id='memusagef' height='15' width='" . (100 - $percentage) . "' border='0' align='absmiddle'>";
+			echo "<img src='bar_right.gif' height='15' width='5' border='0' align='absmiddle'> ";
+			?>
+			<input style="padding: 0; border: 0;" size="30" name="memusage" id="memusage" value="<?=sprintf(gettext("%d%% of %dMB"), $percentage, round($raminfo['physical'] / 1024 / 1024));?>"/>
     </td>
   </tr>
 	<tr>
   	<td width="25%" class="vncellt"><?=gettext("Load averages");?></td>
 		<td width="75%" class="listr">
-			<?
-			exec("uptime", $result); echo substr(strrchr($result[0], "load averages:"),15)." <small>[<a href='status_process.php'>".gettext("show process information")."</a></small>]";
+			<?php
+			exec("uptime", $result);
+			$loadaverage = substr(strrchr($result[0], "load averages:"), 15);
 			?>
+			<input style="padding: 0; border: 0;" size="14" name="loadaverage" id="loadaverage" value="<?=$loadaverage;?>"/>
+			<?="<small>[<a href='status_process.php'>".gettext("Show process information")."</a></small>]";?>
     </td>
   </tr>
 	<tr>
