e44a340d657ea238b17be2d3da8758c36156e8f1
votdev <votdev@localhost>
2008-03-11 11:57:18 +0000

    Adapt WebGUI layout to new style.

--- www/index.php
+++ www/index.php
@@ -45,115 +45,123 @@ $pgtitle_omit = true;
   <tr align="center" valign="top">
     <td height="170" colspan="2"><img src="logobig.gif" width="520" height="149"></td>
   </tr>
+</table>
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
   <tr>
-    <td colspan="2" class="listtopic"><?=gettext("System information");?></td>
-  </tr>
-  <tr>
-    <td width="25%" class="vncellt"><?=gettext("Name");?></td>
-    <td width="75%" class="listr">
-      <?php echo $config['system']['hostname'] . "." . $config['system']['domain']; ?>
-    </td>
-  </tr>
-  <tr>
-    <td width="25%" valign="top" class="vncellt"><?=gettext("Version");?></td>
-    <td width="75%" class="listr">
-      <strong><?=get_product_version();?></strong> (revision <?=get_product_revision();?>)</br>
-			<?=gettext("built on");?> <?=get_product_buildtime();?>
-    </td>
-  </tr>
-  <tr>
-    <td width="25%" valign="top" class="vncellt"><?=gettext("OS Version");?></td>
-    <td width="75%" class="listr">
-      <?
-        exec("/sbin/sysctl -n kern.ostype", $ostype);
-        exec("/sbin/sysctl -n kern.osrelease", $osrelease);
-        exec("/sbin/sysctl -n kern.osrevision", $osrevision);
-        echo("$ostype[0] $osrelease[0] (revision $osrevision[0])");
-      ?>
-    </td>
-  </tr>
-  <tr>
-    <td width="25%" class="vncellt"><?=gettext("Platform");?></td>
-    <td width="75%" class="listr">
-      <?=htmlspecialchars($g['fullplatform']);
-      exec("/sbin/sysctl -n hw.model", $cputype);
-      foreach ($cputype as $cputypel) echo " on " . htmlspecialchars($cputypel);
-      exec("/sbin/sysctl -n hw.clockrate", $clockrate);
-      echo " running at $clockrate[0] MHz";
-      ?>
-    </td>
-  </tr>
-  <tr>
-    <td width="25%" class="vncellt"><?=gettext("Date");?></td>
-    <td width="75%" class="listr">
-      <?php exec("/bin/date", $date); echo htmlspecialchars($date[0]); ?>
-    </td>
-  </tr>
-  <tr>
-    <td width="25%" class="vncellt"><?=gettext("Uptime");?></td>
-    <td width="75%" class="listr">
-			<?=htmlspecialchars(system_get_uptime());?>
-    </td>
-  </tr>
-  <?php if ($config['lastchange']): ?>
-    <tr>
-      <td width="25%" class="vncellt"><?=gettext("Last config change");?></td>
-      <td width="75%" class="listr">
-        <?=htmlspecialchars(date("D M j G:i:s T Y", $config['lastchange']));?>
-      </td>
-    </tr>
-  <?php endif; ?>
-  <tr>
-    <td width="25%" class="vncellt"><?=gettext("Memory usage");?></td>
-    <td width="75%" class="listr">
-      <?php
-				$raminfo = system_get_ram_info();
-        $memUsage = round(($raminfo['used'] * 100) / $raminfo['total'], 0);
-
-        echo "<img src='bar_left.gif' height='15' width='4' border='0' align='absmiddle'>";
-        echo "<img src='bar_blue.gif' height='15' width='" . $memUsage . "' border='0' align='absmiddle'>";
-        echo "<img src='bar_gray.gif' height='15' width='" . (100 - $memUsage) . "' border='0' align='absmiddle'>";
-        echo "<img src='bar_right.gif' height='15' width='5' border='0' align='absmiddle'> ";
-        echo sprintf(gettext("%d%% of %dMB"), $memUsage, round($raminfo['physical'] / 1024 / 1024));
-      ?>
-    </td>
-  </tr>
-	<tr>
-  	<td width="25%" class="vncellt"><?=gettext("Load averages");?></td>
-		<td width="75%" class="listr">
-			<?
-			exec("uptime", $result); echo substr(strrchr($result[0], "load averages:"),15)." <small>[<a href='status_process.php'>".gettext("show process information")."</a></small>]";
-			?>
-    </td>
-  </tr>
-	<tr>
-    <td width="25%" class="vncellt"><?=gettext("Disk space usage");?></td>
-    <td width="75%" class="listr">
-	    <table>
-	      <?php
-	      $diskuse = get_mount_usage();
-	      if (is_array($diskuse) && (0 < count($diskuse))) {
-					foreach ($diskuse as $diskusek => $diskusev) {
-						echo "<tr><td>";
-						$index = array_search_ex($diskusev['filesystem'], $config['mounts']['mount'], "devicespecialfile");
-						echo htmlspecialchars($config['mounts']['mount'][$index]['desc']);
-						echo "</td><td>";
-						$percent_used = rtrim($diskusev['capacity'],"%");
-
-						$tooltip_used = sprintf(gettext("%sB used of %sB"), $diskusev['used'], $diskusev['size']);
-						$tooltip_available = sprintf(gettext("%sB available of %sB"), $diskusev['avail'], $diskusev['size']);
-
-						echo "<img src='bar_left.gif' height='15' width='4' border='0' align='absmiddle'>";
-						echo "<img src='bar_blue.gif' height='15' width='" . $percent_used . "' border='0' align='absmiddle' title='" . $tooltip_used . "'>";
-						echo "<img src='bar_gray.gif' height='15' width='" . (100 - $percent_used) . "' border='0' align='absmiddle' title='" . $tooltip_available . "'>";
-						echo "<img src='bar_right.gif' height='15' width='5' border='0' align='absmiddle'> ";
-						echo sprintf(gettext("%s of %sB"), $diskusev['capacity'], $diskusev['size']);
-						echo "<br></td></tr>";
-					}
-				} else {
-					echo gettext("No disk configured");
-				}
-				?>
+    <td class="tabcont">
+    	<table width="100%" border="0" cellspacing="0" cellpadding="0">
+			  <tr>
+			    <td colspan="2" class="listtopic"><?=gettext("System information");?></td>
+			  </tr>
+			  <tr>
+			    <td width="25%" class="vncellt"><?=gettext("Name");?></td>
+			    <td width="75%" class="listr">
+			      <?php echo $config['system']['hostname'] . "." . $config['system']['domain']; ?>
+			    </td>
+			  </tr>
+			  <tr>
+			    <td width="25%" valign="top" class="vncellt"><?=gettext("Version");?></td>
+			    <td width="75%" class="listr">
+			      <strong><?=get_product_version();?></strong> (revision <?=get_product_revision();?>)</br>
+						<?=gettext("built on");?> <?=get_product_buildtime();?>
+			    </td>
+			  </tr>
+			  <tr>
+			    <td width="25%" valign="top" class="vncellt"><?=gettext("OS Version");?></td>
+			    <td width="75%" class="listr">
+			      <?
+			        exec("/sbin/sysctl -n kern.ostype", $ostype);
+			        exec("/sbin/sysctl -n kern.osrelease", $osrelease);
+			        exec("/sbin/sysctl -n kern.osrevision", $osrevision);
+			        echo("$ostype[0] $osrelease[0] (revision $osrevision[0])");
+			      ?>
+			    </td>
+			  </tr>
+			  <tr>
+			    <td width="25%" class="vncellt"><?=gettext("Platform");?></td>
+			    <td width="75%" class="listr">
+			      <?=htmlspecialchars($g['fullplatform']);
+			      exec("/sbin/sysctl -n hw.model", $cputype);
+			      foreach ($cputype as $cputypel) echo " on " . htmlspecialchars($cputypel);
+			      exec("/sbin/sysctl -n hw.clockrate", $clockrate);
+			      echo " running at $clockrate[0] MHz";
+			      ?>
+			    </td>
+			  </tr>
+			  <tr>
+			    <td width="25%" class="vncellt"><?=gettext("Date");?></td>
+			    <td width="75%" class="listr">
+			      <?php exec("/bin/date", $date); echo htmlspecialchars($date[0]); ?>
+			    </td>
+			  </tr>
+			  <tr>
+			    <td width="25%" class="vncellt"><?=gettext("Uptime");?></td>
+			    <td width="75%" class="listr">
+						<?=htmlspecialchars(system_get_uptime());?>
+			    </td>
+			  </tr>
+			  <?php if ($config['lastchange']): ?>
+			    <tr>
+			      <td width="25%" class="vncellt"><?=gettext("Last config change");?></td>
+			      <td width="75%" class="listr">
+			        <?=htmlspecialchars(date("D M j G:i:s T Y", $config['lastchange']));?>
+			      </td>
+			    </tr>
+			  <?php endif; ?>
+			  <tr>
+			    <td width="25%" class="vncellt"><?=gettext("Memory usage");?></td>
+			    <td width="75%" class="listr">
+			      <?php
+							$raminfo = system_get_ram_info();
+			        $memUsage = round(($raminfo['used'] * 100) / $raminfo['total'], 0);
+			
+			        echo "<img src='bar_left.gif' height='15' width='4' border='0' align='absmiddle'>";
+			        echo "<img src='bar_blue.gif' height='15' width='" . $memUsage . "' border='0' align='absmiddle'>";
+			        echo "<img src='bar_gray.gif' height='15' width='" . (100 - $memUsage) . "' border='0' align='absmiddle'>";
+			        echo "<img src='bar_right.gif' height='15' width='5' border='0' align='absmiddle'> ";
+			        echo sprintf(gettext("%d%% of %dMB"), $memUsage, round($raminfo['physical'] / 1024 / 1024));
+			      ?>
+			    </td>
+			  </tr>
+				<tr>
+			  	<td width="25%" class="vncellt"><?=gettext("Load averages");?></td>
+					<td width="75%" class="listr">
+						<?
+						exec("uptime", $result); echo substr(strrchr($result[0], "load averages:"),15)." <small>[<a href='status_process.php'>".gettext("show process information")."</a></small>]";
+						?>
+			    </td>
+			  </tr>
+				<tr>
+			    <td width="25%" class="vncellt"><?=gettext("Disk space usage");?></td>
+			    <td width="75%" class="listr">
+				    <table>
+				      <?php
+				      $diskuse = get_mount_usage();
+				      if (is_array($diskuse) && (0 < count($diskuse))) {
+								foreach ($diskuse as $diskusek => $diskusev) {
+									echo "<tr><td>";
+									$index = array_search_ex($diskusev['filesystem'], $config['mounts']['mount'], "devicespecialfile");
+									echo htmlspecialchars($config['mounts']['mount'][$index]['desc']);
+									echo "</td><td>";
+									$percent_used = rtrim($diskusev['capacity'],"%");
+			
+									$tooltip_used = sprintf(gettext("%sB used of %sB"), $diskusev['used'], $diskusev['size']);
+									$tooltip_available = sprintf(gettext("%sB available of %sB"), $diskusev['avail'], $diskusev['size']);
+			
+									echo "<img src='bar_left.gif' height='15' width='4' border='0' align='absmiddle'>";
+									echo "<img src='bar_blue.gif' height='15' width='" . $percent_used . "' border='0' align='absmiddle' title='" . $tooltip_used . "'>";
+									echo "<img src='bar_gray.gif' height='15' width='" . (100 - $percent_used) . "' border='0' align='absmiddle' title='" . $tooltip_available . "'>";
+									echo "<img src='bar_right.gif' height='15' width='5' border='0' align='absmiddle'> ";
+									echo sprintf(gettext("%s of %sB"), $diskusev['capacity'], $diskusev['size']);
+									echo "<br></td></tr>";
+								}
+							} else {
+								echo gettext("No disk configured");
+							}
+							?>
+						</table>
+					</td>
+				</tr>
 			</table>
 		</td>
 	</tr>
