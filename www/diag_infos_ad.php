780f77df4326b954f6063e1577452f08691c87e7
votdev <votdev@localhost>
2008-03-29 13:07:37 +0000

    List imported AD users.

--- www/diag_infos_ad.php
+++ www/diag_infos_ad.php
@@ -57,22 +57,21 @@ if (!is_array($config['ad'])) {
 				<li class="tabinact"><a href="diag_infos_rsync_client.php"><?=gettext("RSYNC Client");?></a></li>
 				<li class="tabinact"><a href="diag_infos_swap.php"><?=gettext("Swap");?></a></li>
 				<li class="tabinact"><a href="diag_infos_sockets.php"><?=gettext("Sockets");?></a></li>
-				<li class="tabinact"><a href="diag_infos_sensors.php"><?=gettext("Sensors");?></a></li>
 			</ul>
 		</td>
 	</tr>
-  <tr>
-    <td class="tabcont">
-    	<table width="100%" border="0">
+	<tr>
+		<td class="tabcont">
+			<table width="100%" border="0">
 				<tr>
 					<td class="listtopic"><?=gettext("MS Domain informations");?></td>
 				</tr>
 				<tr>
-			    <td>
-			    	<?php if (!isset($config['ad']['enable'])):?>
-			    	<pre><br/><?=gettext("AD authentication disabled");?></pre>
-			    	<?php else:?>
-			    	<pre><br/><?php
+					<td>
+						<?php if (!isset($config['ad']['enable'])):?>
+						<pre><br/><?=gettext("AD authentication disabled");?></pre>
+						<?php else:?>
+						<pre><br/><?php
 						echo "<strong>".gettext("Accessiblity test to MS domain").":</strong><br><br>";
 						echo gettext("Results for net rpc testjoin:") . "<br>";
 						exec("/usr/local/bin/net rpc testjoin", $rawdata);
@@ -87,11 +86,23 @@ if (!is_array($config['ad'])) {
 						echo implode("\n", $rawdata);
 						unset($rawdata);
 						?></pre>
+					</td>
+				</tr>
+				<tr>
+					<td class="listtopic"><?=gettext("List of imported users");?></td>
+				</tr>
+				<tr>
+					<td>
+						<pre><br/><?php
+						exec("/usr/local/bin/net rpc user -S {$config['ad']['ad_srv_name']} -U {$config['ad']['admin_name']}%{$config['ad']['admin_pass']}", $rawdata);
+						echo implode("\n", $rawdata);
+						unset($rawdata);
+						?></pre>
 						<?php endif;?>
 					</td>
-			  </tr>
-    	</table>
-    </td>
-  </tr>
+				</tr>
+			</table>
+		</td>
+	</tr>
 </table>
 <?php include("fend.inc");?>
