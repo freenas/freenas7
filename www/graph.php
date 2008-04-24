54b5bec1dfdfc41f7d61ca089ab483a4fe990563
votdev <votdev@localhost>
2008-04-24 11:19:01 +0000

    Fix initial view of the the bandwidth graph to bytes/s (BR 1948447). Thanks to Robert Eden.

--- www/graph.php
+++ www/graph.php
@@ -89,8 +89,8 @@ echo "<?xml version=\"1.0\" encoding=\"{$encoding}\"?>\n";
     <text id="graph_in_lbl" x="5" y="8" <?=$attribs['in']?>><?=gettext("In");?> <tspan id="graph_in_txt" <?=$attribs['in']?>> </tspan></text>
     <text id="graph_out_lbl" x="5" y="16" <?=$attribs['out']?>><?=gettext("Out");?> <tspan id="graph_out_txt" <?=$attribs['out']?>> </tspan></text>
     <text id="ifname" x="<?=$width?>" y="8" <?=$attribs['graphname']?> text-anchor="end"><?=$ifname?></text>
-    <text id="switch_unit" x="<?=$width*0.55?>" y="5" <?=$attribs['switch_unit']?>><?=gettext("Switch to") . " " . $unit . "/s";?></text>
-    <text id="switch_scale" x="<?=$width*0.55?>" y="11" <?=$attribs['switch_scale']?>><?=gettext("AutoScale");?> (<?=($scale_type == 'up') ? gettext("Up") : gettext("Follow");?>)</text>
+    <text id="switch_unit" x="<?=$width*0.55?>" y="5" <?=$attribs['switch_unit']?>><?=sprintf(gettext("Switch to %s/s"), ("bits" === $unit) ? "bytes" : "bits");?></text>
+    <text id="switch_scale" x="<?=$width*0.55?>" y="11" <?=$attribs['switch_scale']?>><?=gettext("AutoScale");?> (<?=("up" === $scale_type) ? gettext("Up") : gettext("Follow");?>)</text>
     <text id="datetime" x="<?=$width*0.55?>" y="17" <?=$attribs['legend']?>> </text>
     <text id="graphlast" x="<?=$width*0.55?>" y="23" <?=$attribs['legend']?>><?=gettext("Graph shows last");?> <?=$time_interval*$nb_plot?> <?=gettext("seconds");?></text>
     <polygon id="axis_arrow_x" <?=$attribs['axis']?> points="<?=($width) . "," . ($height)?> <?=($width-2) . "," . ($height-2)?> <?=($width-2) . "," . $height?>"/>
