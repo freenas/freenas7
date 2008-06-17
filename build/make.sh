62a619e344533fde1ddb0bdff4230fd5d779c3af
votdev <votdev@localhost>
2008-06-17 06:27:10 +0000

    Increase mfsroot size form AMD64.

--- build/make.sh
+++ build/make.sh
@@ -47,7 +47,7 @@ FREENAS_SVNURL="https://freenas.svn.sourceforge.net/svnroot/freenas/branches/0.6
 # and FreeNAS WEbGUI/Scripts. Keep this file very small! This file is unzipped
 # to a RAM disk at FreeNAS startup.
 if [ "amd64" = ${FREENAS_ARCH} ]; then
-FREENAS_MFSROOT_SIZE=60
+FREENAS_MFSROOT_SIZE=62
 FREENAS_IMG_SIZE=30
 else
 FREENAS_MFSROOT_SIZE=56
