d8f00c85498c51878789ac6b23c7bae4518739f4
votdev <votdev@localhost>
2008-06-29 17:31:41 +0000

    Modity iSCSI-Target WebGUI. This will hopefully reduce 'Extent' configuration problems.

--- www/services_iscsitarget_extent_edit.php
+++ www/services_iscsitarget_extent_edit.php
@@ -53,6 +53,11 @@ if (isset($id) && $a_iscsitarget_extent[$id]) {
 	$pconfig['name'] = $a_iscsitarget_extent[$id]['name'];
 	$pconfig['path'] = $a_iscsitarget_extent[$id]['path'];
 	$pconfig['size'] = $a_iscsitarget_extent[$id]['size'];
+
+	// Check if a device is used as target.
+	$pconfig['type'] = "device";
+	if (!preg_match("/^\/dev\/(\S+)/", $pconfig['path']))
+		$pconfig['type'] = "file";
 } else {
 	// Find next unused ID.
 	$extentid = 0;
@@ -65,6 +70,7 @@ if (isset($id) && $a_iscsitarget_extent[$id]) {
 	$pconfig['name'] = "extent{$extentid}";
 	$pconfig['path'] = "";
 	$pconfig['size'] = "";
+	$pconfig['type'] = "device";
 }
 
 if ($_POST) {
@@ -74,18 +80,30 @@ if ($_POST) {
 	$pconfig = $_POST;
 
 	// Input validation.
-	$reqdfields = explode(" ", "path size");
-	$reqdfieldsn = array(gettext("Path"),gettext("File size"));
-	$reqdfieldst = explode(" ", "string numeric");
-	
+	if ("device" === $_POST['type']) {
+		$reqdfields = explode(" ", "device");
+		$reqdfieldsn = array(gettext("Device"));
+		$reqdfieldst = explode(" ", "string");
+	} else {
+		$reqdfields = explode(" ", "path size");
+		$reqdfieldsn = array(gettext("Path"),gettext("File size"));
+		$reqdfieldst = explode(" ", "string numeric");
+	}
+
 	do_input_validation($_POST, $reqdfields, $reqdfieldsn, &$input_errors);
 	do_input_validation_type($_POST, $reqdfields, $reqdfieldsn, $reqdfieldst, &$input_errors);
 
 	if (!$input_errors) {
 		$iscsitarget_extent = array();
 		$iscsitarget_extent['name'] = $_POST['name'];
-		$iscsitarget_extent['path'] = $_POST['path'];
-		$iscsitarget_extent['size'] = $_POST['size'];
+		if ("device" === $_POST['type']) {
+			$diskinfo = disks_get_diskinfo($_POST['device']);
+			$iscsitarget_extent['path'] = $diskinfo['name'];
+			$iscsitarget_extent['size'] = $diskinfo['mediasize_mbytes'] - 1; // Calculate target size.
+		} else {
+			$iscsitarget_extent['path'] = $_POST['path'];
+			$iscsitarget_extent['size'] = $_POST['size'];
+		}
 
 		if (isset($id) && $a_iscsitarget_extent[$id])
 			$a_iscsitarget_extent[$id] = $iscsitarget_extent;
@@ -102,6 +120,25 @@ if ($_POST) {
 }
 ?>
 <?php include("fbegin.inc");?>
+<script language="JavaScript">
+<!--
+function type_change() {
+	switch (document.iform.type.value) {
+	case "file":
+		showElementById('path_tr','show');
+		showElementById('size_tr','show');
+		showElementById('device_tr','hide');
+		break;
+	
+	case "device":
+		showElementById('path_tr','hide');
+		showElementById('size_tr','hide');
+		showElementById('device_tr','show');
+		break;
+	}
+}
+// -->
+</script>
 <form action="services_iscsitarget_extent_edit.php" method="post" name="iform" id="iform">
 	<table width="100%" border="0" cellpadding="0" cellspacing="0">
 		<tr>
@@ -109,7 +146,10 @@ if ($_POST) {
 				<?php if ($input_errors) print_input_errors($input_errors);?>
 				<table width="100%" border="0" cellpadding="6" cellspacing="0">
 					<?php html_inputbox("name", gettext("Extent name"), $pconfig['name'], "", true, 10, isset($id));?>
+					<?php html_combobox("type", gettext("Type"), $pconfig['type'], array("file" => gettext("File"), "device" => gettext("Device")), "", true, false, "type_change()");?>
 					<?php html_filechooser("path", "Path", $pconfig['path'], sprintf(gettext("File path (e.g. /mnt/sharename/extent/%s) or device name (e.g. /dev/ad1) used as extent."), $pconfig['name']), "/mnt", true);?>
+					<?php $a_device = array(); $a_device[''] = gettext("Must choose one"); foreach (get_conf_all_disks_list_filtered() as $diskv) { if (0 == strcmp($diskv['size'], "NA")) continue; if (1 == disks_exists($diskv['devicespecialfile'])) continue; $diskinfo = disks_get_diskinfo($diskv['devicespecialfile']); $a_device[$diskv['devicespecialfile']] = htmlspecialchars("{$diskv['name']}: {$diskinfo['mediasize_mbytes']}MB ({$diskv['desc']})"); }?>
+					<?php html_combobox("device", gettext("Device"), $pconfig['path'], $a_device, "", true);?>
 					<?php html_inputbox("size", gettext("File size"), $pconfig['size'], gettext("Size in MB."), true, 10);?>
 					<tr>
 						<td width="22%" valign="top">&nbsp;</td>
@@ -124,4 +164,9 @@ if ($_POST) {
 		</tr>
 	</table>
 </form>
+<script language="JavaScript">
+<!--
+type_change(false);
+//-->
+</script>
 <?php include("fend.inc");?>
