77fd6ddb3d55387777ff726c88aa937dc5ea3208
votdev <votdev@localhost>
2008-07-29 16:40:26 +0000

    Modify filechooser CSS code.

--- www/filechooser.php
+++ www/filechooser.php
@@ -507,6 +507,8 @@ EOD;
 		<script type="text/javascript" src="niftycube/niftycube.js"></script>
 		<script type="text/javascript" src="niftycube/niftylayout.js"></script>
 		<style type="text/css">
+			body { background: #FFFFFF; }
+
 			.filechooser { background-color: #fff; margin: 0px; padding: 0px; }
 			.filechooser table { width: 100%; height: 100%; font-size: 11px; font-family: Tahoma, Verdana, Arial, sans-serif !important; }
 			.filechooser td { padding: 10px; vertical-align: top; }
