67ec9ddeae48d1772f73168368c3ab364f2ff3e1
votdev <votdev@localhost>
2008-08-24 16:46:40 +0000

    Add 'System|UPS' WebGUI. This is a really basic one. We need the help of the community here to make the UI better.

--- www/diag_infos_smart.php
+++ www/diag_infos_smart.php
@@ -55,6 +55,7 @@ $a_disk = get_physical_disks_list();
 				<li class="tabinact"><a href="diag_infos_rsync_client.php"><span><?=gettext("RSYNC Client");?></span></a></li>
 				<li class="tabinact"><a href="diag_infos_swap.php"><span><?=gettext("Swap");?></span></a></li>
 				<li class="tabinact"><a href="diag_infos_sockets.php"><span><?=gettext("Sockets");?></span></a></li>
+				<li class="tabinact"><a href="diag_infos_ups.php"><span><?=gettext("UPS");?></span></a></li>
 			</ul>
   	</td>
 	</tr>
