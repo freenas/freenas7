3b321dfa743252ec91d08388d80eb5160d7d9bf4
votdev <votdev@localhost>
2008-09-11 12:55:53 +0000

    Increase mfs_root size to 74MB (32bit) and 80MB (64bit).

--- build/make.sh
+++ build/make.sh
@@ -47,11 +47,11 @@ FREENAS_SVNURL="https://freenas.svn.sourceforge.net/svnroot/freenas/branches/0.6
 # and FreeNAS WEbGUI/Scripts. Keep this file very small! This file is unzipped
 # to a RAM disk at FreeNAS startup.
 if [ "amd64" = ${FREENAS_ARCH} ]; then
-FREENAS_MFSROOT_SIZE=76
-FREENAS_IMG_SIZE=36
+FREENAS_MFSROOT_SIZE=80
+FREENAS_IMG_SIZE=40
 else
-FREENAS_MFSROOT_SIZE=70
-FREENAS_IMG_SIZE=32
+FREENAS_MFSROOT_SIZE=74
+FREENAS_IMG_SIZE=36
 fi
 
 # Media geometry, only relevant if bios doesn't understand LBA.
