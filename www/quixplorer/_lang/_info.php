0e5a1b7c592f1be08e4bf0ca50506cd91f65026e
votdev <votdev@localhost>
2008-10-05 17:19:13 +0000

    Add quixplorer 2.3.1 to administrate file system via web browser. It is accessible via http://xxx.xxx.xxx.xxx/quixplorer. Default user/password is 'admin/freenas'.

---
+++ www/quixplorer/_lang/_info.php
@@ -0,0 +1,6 @@
+<OPTION value="en">English</OPTION>
+<OPTION value="de">Deutsch</OPTION>
+<OPTION value="es">Español</OPTION>
+<OPTION value="fr">Français</OPTION>
+<OPTION value="nl">Nederlands</OPTION>
+<OPTION value="ru">Russian</OPTION>
