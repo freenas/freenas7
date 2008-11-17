518bec33ad5d7164918b6a5b3423effc4b395958
votdev <votdev@localhost>
2008-11-17 19:51:41 +0000

    Add Fuppes patches to modify WebGUI (adopt colors to FreeNAS look, changed capitalization of misc texts). Thanks to Dutchman01.

--- build/ports/fuppes/files/patch-src_lib_Presentation_PresentationHandler.cpp
+++ build/ports/fuppes/files/patch-src_lib_Presentation_PresentationHandler.cpp
@@ -1,5 +1,5 @@
 --- src/lib/Presentation/PresentationHandler.cpp.orig	2007-11-23 07:56:59.000000000 +0000
-+++ src/lib/Presentation/PresentationHandler.cpp	2008-11-15 20:15:56.000000000 +0000
++++ src/lib/Presentation/PresentationHandler.cpp	2008-11-17 19:41:51.000000000 +0000
 @@ -3,7 +3,7 @@
   *
   *  FUPPES - Free UPnP Entertainment Service
@@ -9,7 +9,15 @@
   ****************************************************************************/
  
  /*
-@@ -324,7 +324,7 @@
+@@ -276,7 +276,6 @@
+   sResult << "<br />";
+   sResult << "<a href=\"/presentation/config.html\">Configuration</a>" << endl;    
+   sResult << "<br />";
+-  sResult << "Debug" << endl;
+   
+   sResult << "</div>" << endl;  
+   /* menu end */
+@@ -324,7 +323,7 @@
  {
    std::stringstream sResult;
    
@@ -18,7 +26,7 @@
    
    sResult << "<p>" << endl;
    sResult << "Version: " << CSharedConfig::Shared()->GetAppVersion() << "<br />" << endl;
-@@ -334,8 +334,8 @@
+@@ -334,8 +333,8 @@
    sResult << "</p>" << endl;
    
    sResult << "<p>" << endl;
@@ -29,7 +37,7 @@
    sResult << "</p>" << endl;
    
    sResult << "<p>" << endl;
-@@ -343,7 +343,7 @@
+@@ -343,7 +342,7 @@
    sResult << "</p>" << endl;
    
    
@@ -38,7 +46,7 @@
    sResult << BuildFuppesDeviceList(CSharedConfig::Shared()->GetFuppesInstance(0), p_sImgPath);
    
    return sResult.str();
-@@ -382,17 +382,17 @@
+@@ -382,17 +381,17 @@
    /*sResult << "<a href=\"http://sourceforge.net/projects/fuppes/\">http://sourceforge.net/projects/fuppes/</a><br />" << endl; */
  
    //((CFuppes*)m_vFuppesInstances[0])->GetContentDirectory()->BuildDB();
@@ -62,7 +70,7 @@
  	}
    
    return sResult.str();
-@@ -413,7 +413,7 @@
+@@ -413,7 +412,7 @@
    OBJECT_TYPE nType = OBJECT_TYPE_UNKNOWN;  
    
    // Database status
@@ -71,7 +79,7 @@
    sResult << 
      "<table rules=\"all\" style=\"font-size: 10pt; border-style: solid; border-width: 1px; border-color: #000000;\" cellspacing=\"0\" width=\"400\">" << endl <<
        "<thead>" << endl <<
-@@ -448,13 +448,13 @@
+@@ -448,13 +447,13 @@
    
    
    string sTranscoding;
@@ -87,7 +95,7 @@
    "<table>" <<    
      "<tr>" <<
        "<th>option</th>" <<
-@@ -565,7 +565,7 @@
+@@ -565,7 +564,7 @@
    "</table>";    
    
    // system status
@@ -96,7 +104,7 @@
    
    sResult << "<p>" << endl;
    sResult << "UUID: " << CSharedConfig::Shared()->GetFuppesInstance(0)->GetUUID() << "<br />";    
-@@ -574,7 +574,7 @@
+@@ -574,7 +573,7 @@
    
    
    // device settings
@@ -105,7 +113,12 @@
    string sDeviceSettings;
    CDeviceIdentificationMgr::Shared()->PrintSettings(&sDeviceSettings);
    sResult << sDeviceSettings << endl;
-@@ -676,7 +676,7 @@
+@@ -672,11 +671,11 @@
+   sResult << "Please edit the file: " << CSharedConfig::Shared()->GetConfigFileName() << "</strong>" << endl;
+   
+   /* show config page */
+-  sResult << "<h1>ContentDirectory settings</h1>" << endl;
++  sResult << "<h1>Content directory settings</h1>" << endl;
    sResult << "<form method=\"POST\" action=\"/presentation/config.html\" enctype=\"text/plain\" accept-charset=\"UTF-8\">" << endl;  //  
    
    // shared objects
@@ -114,3 +127,30 @@
    
    // object list
    sResult << "<p>" << endl <<  
+@@ -741,7 +740,7 @@
+   
+              
+   // charset
+-  sResult << "<h2>character encoding</h2>" << endl;
++  sResult << "<h2>Character encoding</h2>" << endl;
+   sResult << "<p>Set your local character encoding.<br />" <<
+              "<a href=\"http://www.gnu.org/software/libiconv/\" target=\"blank\">http://www.gnu.org/software/libiconv/</a><br />" << endl <<
+              "</p>" << endl;
+@@ -753,7 +752,7 @@
+              
+   
+   // max filename length
+-  /*sResult << "<h2>max file name length</h2>" << endl;
++  /*sResult << "<h2>Max file name length</h2>" << endl;
+   sResult << "<p>The \"max file name length\" option sets the maximum length for file names in the directory listings.<br />" <<
+              "some devices can't handle an unlimited length.<br />" << endl <<
+              "(e.g. the Telegent TG 100 crashes on receiving file names larger then 101 characters.)<br />" << endl <<
+@@ -781,7 +780,7 @@
+   
+   // allowed ip list
+   sResult << "<p>" << endl <<  
+-             "host address is always allowed to access." << endl <<
++             "Host address is always allowed to access." << endl <<
+              "</p>" << endl <<
+              "<p>" << endl <<
+              "<table>" << endl <<
