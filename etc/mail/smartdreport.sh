a5264601854d5066176eb9de74f89286f07b8ed0
votdev <votdev@localhost>
2008-12-05 20:52:21 +0000

    Introduce email.subr framework. Use send_email function in all scripts.

--- etc/mail/smartdreport.sh
+++ etc/mail/smartdreport.sh
@@ -3,33 +3,15 @@
 # All rights reserved.
 
 . /etc/rc.subr
-. /etc/configxml.subr
+. /etc/email.subr
 
-# Defaults
-msmtp_config=${msmtp_config:-"/var/etc/msmtp.conf"}
-msmtp_msgfile=${msmtp_msgfile:-"/tmp/message"}
+name="smartdreport"
 
-# Get date in RFC 2882 format.
-_rfcdate=`date "+%a, %d %b %Y %H:%M:%S %z"`
+load_rc_config "${name}"
 
-# Create message
-echo ${SMARTD_ADDRESS} | awk '{for ( i = NF ; i > 0 ; --i ) printf("To: %s\n",$i)}' > ${msmtp_msgfile}
+# Send output of smartctl -a to as message.
+_message=`cat`
+_report=`/usr/local/sbin/smartctl -a -d ${SMARTD_DEVICETYPE} ${SMARTD_DEVICE}`
 
-/usr/local/bin/xml sel -t \
-	-v "concat('From: ',//system/email/from)" -n \
-	-o "Subject: ${SMARTD_SUBJECT}" -n \
-	-o "Date: ${_rfcdate}" -n \
-	-o "." -n \
-	${configxml_file} | /usr/local/bin/xml unesc >> ${msmtp_msgfile}
-
-# Save the email message (STDIN) to a file:
-cat >> ${msmtp_msgfile}
-
-# Append the output of smartctl -a to the message:
-/usr/local/sbin/smartctl -a -d ${SMARTD_DEVICETYPE} ${SMARTD_DEVICE} 1>> ${msmtp_msgfile}
-
-# Now email the message to the user at address ADD:
-/usr/local/bin/msmtp --file=${msmtp_config} ${SMARTD_ADDRESS} < ${msmtp_msgfile} 1>/dev/null 2>&1
-
-# Cleanup
-/bin/rm ${msmtp_msgfile} 1>/dev/null 2>&1
+# Send email.
+send_email "${SMARTD_ADDRESS}" "${SMARTD_SUBJECT}" "${_message} ${_report}"
