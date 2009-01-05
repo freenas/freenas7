491ed65c917e0ff3ab8c952d7adb53307cab8c4c
votdev <votdev@localhost>
2009-01-05 22:10:01 +0000

    Try to fix 'cannot get cpu load' graph error message (BR 2486326).

--- www/stats.php
+++ www/stats.php
@@ -1,9 +1,9 @@
 #!/usr/local/bin/php
-<?php 
+<?php
 /*
 	status_graph.php
 	part of FreeNAS (http://www.freenas.org)
-	Copyright (C) 2006-2008 Volker Theile <votdev@gmx.de>.
+	Copyright (C) 2006-2009 Volker Theile <votdev@gmx.de>.
 	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
@@ -11,7 +11,7 @@
 
 	1. Redistributions of source code must retain the above copyright notice,
 	   this list of conditions and the following disclaimer.
-	
+
 	2. Redistributions in binary form must reproduce the above copyright
 	   notice, this list of conditions and the following disclaimer in the
 	   documentation and/or other materials provided with the distribution.
@@ -29,15 +29,15 @@
 */
 require("guiconfig.inc");
 
-if(0 == strcmp("cpu",getenv(QUERY_STRING))) {
-  $cpuload = system_get_cpu_usage();
-  echo $cpuload;
+// Make sure no other output than the requested data is echoed.
+if (0 == strcmp("cpu", getenv(QUERY_STRING))) {
+	$cpuload = @system_get_cpu_usage();
+	echo $cpuload;
 } else {
-  $if = $_GET['if'];
-  $ifinfo = get_interface_info($if);
-  $time = gettimeofday();
-  $timing = (double)$time["sec"] + (double)$time["usec"] / 1000000.0;
-
-  echo "$timing|" . $ifinfo['inbytes'] . "|" . $ifinfo['outbytes'] . "\n";
+	$if = $_GET['if'];
+	$ifinfo = @get_interface_info($if);
+	$time = gettimeofday();
+	$timing = (double)$time["sec"] + (double)$time["usec"] / 1000000.0;
+	echo "$timing|" . $ifinfo['inbytes'] . "|" . $ifinfo['outbytes'] . "\n";
 }
 ?>
