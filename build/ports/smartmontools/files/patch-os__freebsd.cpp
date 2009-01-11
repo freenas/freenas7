a9c3077548aabd2459059b444c2270403c05070f
votdev <votdev@localhost>
2009-01-11 03:48:57 +0000

    Add smartctl '-n POWERMODE' patch (FR 2498760).

--- build/ports/smartmontools/files/patch-os__freebsd.cpp
+++ build/ports/smartmontools/files/patch-os__freebsd.cpp
@@ -1,5 +1,5 @@
---- os_freebsd.cpp.orig	2008-03-04 23:09:47.000000000 +0100
-+++ os_freebsd.cpp	2008-03-11 14:23:07.000000000 +0100
+--- os_freebsd.cpp.orig	2009-01-11 03:42:16.000000000 +0000
++++ os_freebsd.cpp	2009-01-11 03:42:18.000000000 +0000
 @@ -272,7 +272,6 @@
  }
  
@@ -8,7 +8,39 @@
    return -1;
  }
  
-@@ -924,6 +923,7 @@
+@@ -391,6 +390,20 @@
+     request.u.ata.lba=0xc24f<<8;
+     request.flags=ATA_CMD_CONTROL;
+     break;
++  case CHECK_POWER_MODE:
++    request.u.ata.command=ATA_CHECK_POWER_MODE;
++    request.u.ata.feature=0;
++    request.flags=ATA_CMD_CONTROL;
++    break;
++  case WRITE_LOG:
++    memcpy(buff, data, 512);
++    request.u.ata.feature=ATA_SMART_WRITE_LOG_SECTOR;
++    request.u.ata.lba=select|(0xc24f<<8);
++    request.u.ata.count=1;
++    request.flags=ATA_CMD_WRITE;
++    request.data=(char *)buff;
++    request.count=512;
++    break;
+   default:
+     pout("Unrecognized command %d in ata_command_interface()\n"
+          "Please contact " PACKAGE_BUGREPORT "\n", command);
+@@ -448,6 +461,10 @@
+     return -1;
+   }
+   // 
++  if (command == CHECK_POWER_MODE) {
++    data[0] = request.u.ata.count & 0xff;
++    return 0;
++  }
+   if (copydata)
+     memcpy(data, buff, 512);
+   
+@@ -924,6 +941,7 @@
  static const char * fbsd_dev_prefix = "/dev/";
  static const char * fbsd_dev_ata_disk_prefix = "ad";
  static const char * fbsd_dev_scsi_disk_plus = "da";
@@ -16,7 +48,7 @@
  static const char * fbsd_dev_scsi_tape1 = "sa";
  static const char * fbsd_dev_scsi_tape2 = "nsa";
  static const char * fbsd_dev_scsi_tape3 = "esa";
-@@ -960,6 +960,11 @@
+@@ -960,6 +978,11 @@
      return CONTROLLER_ATA;
    }
    
