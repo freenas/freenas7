2bb541acab2de39a1667e13c3e4a582ca60a8041
votdev <votdev@localhost>
2009-02-06 19:23:33 +0000

    Add 'Remote monitors' to 'Services|UPS' to allow adding remote networks that monitor the local installed UPS in slave mode. UPS Service improvements/fixes (BR 2549047).

--- www/services_ups.php
+++ www/services_ups.php
@@ -2,11 +2,11 @@
 <?php
 /*
 	services_ups.php
-	Copyright © 2008 Volker Theile (votdev@gmx.de)
+	Copyright © 2008-2009 Volker Theile (votdev@gmx.de)
 	All rights reserved.
 
 	part of FreeNAS (http://www.freenas.org)
-	Copyright (C) 2005-2008 Olivier Cochard <olivier@freenas.org>.
+	Copyright (C) 2005-2009 Olivier Cochard <olivier@freenas.org>.
 	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
@@ -45,6 +45,7 @@ $pconfig['port'] = $config['ups']['port'];
 $pconfig['desc'] = $config['ups']['desc'];
 $pconfig['shutdownmode'] = $config['ups']['shutdownmode'];
 $pconfig['shutdowntimer'] = $config['ups']['shutdowntimer'];
+$pconfig['remotemonitors'] = $config['ups']['remotemonitors'];
 $pconfig['email_enable'] = isset($config['ups']['email']['enable']);
 $pconfig['email_to'] = $config['ups']['email']['to'];
 $pconfig['email_subject'] = $config['ups']['email']['subject'];
@@ -90,6 +91,7 @@ if ($_POST) {
 		$config['ups']['desc'] = $_POST['desc'];
 		$config['ups']['shutdownmode'] = $_POST['shutdownmode'];
 		$config['ups']['shutdowntimer'] = $_POST['shutdowntimer'];
+		$config['ups']['remotemonitors'] = $_POST['remotemonitors'];
 		$config['ups']['email']['enable'] = $_POST['email_enable'] ? true : false;
 		$config['ups']['email']['to'] = $_POST['email_to'];
 		$config['ups']['email']['subject'] = $_POST['email_subject'];
@@ -136,6 +138,7 @@ function enable_change(enable_change) {
 		document.iform.desc.disabled = endis;
 		document.iform.shutdownmode.disabled = endis;
 		document.iform.shutdowntimer.disabled = endis;
+		document.iform.remotemonitors.disabled = endis;
 		document.iform.email_enable.disabled = endis;
 
 		if (document.iform.enable.checked == true) {
@@ -176,6 +179,7 @@ function shutdownmode_change() {
 					<?php html_inputbox("desc", gettext("Description"), $pconfig['desc'], gettext("You may enter a description here for your reference."), false, 40);?>
 					<?php html_combobox("shutdownmode", gettext("Shutdown mode"), $pconfig['shutdownmode'], array("fsd" => gettext("UPS reaches low battery"), "onbatt" => "UPS goes on battery"), gettext("Defines when the shutdown is initiated."), true, false, "shutdownmode_change()");?>
 					<?php html_inputbox("shutdowntimer", gettext("Shutdown timer"), $pconfig['shutdowntimer'], gettext("The time in seconds until shutdown is initiated. If the UPS happens to come back before the time is up the shutdown is canceled."), true, 3);?>
+					<?php html_inputbox("remotemonitors", gettext("Remote monitors"), $pconfig['remotemonitors'], gettext("Enter the network of the remote monitors that monitor this UPS in slave mode, e.g. 192.168.50.0/255.255.255.0 (IPv4), 192.168.50.0/24 (IPv4) or 2001:0db8:1234::/48 (IPv6). Seperate networks by blank."), false, 60);?>
 					<?php html_separator();?>
 					<?php html_titleline_checkbox("email_enable", gettext("Email notification"), $pconfig['email_enable'] ? true : false, gettext("Activate"), "enable_change(this)");?>
 					<?php html_inputbox("email_to", gettext("To email"), $pconfig['email_to'], sprintf("%s %s", gettext("Destination email address."), gettext("Separate email addresses by semi-colon.")), true, 40);?>
