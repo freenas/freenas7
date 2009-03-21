d450838f9be5b20a60ed046d137efe9bc0dfa7ce
votdev <votdev@localhost>
2009-03-21 20:29:32 +0000

    Repo cleanup.

--- build/kernel-patches/rm-patched-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Extract gettext strings from source.
-# Created: 09.09.2007 by Volker Theile (votdev@gmx.de)
-
-echo "Removing patched files..."
-
-for file in $(find /usr/src -name "*.orig"); do
-	rm -rv ${file}
-done
