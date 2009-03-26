fd124f00a717a92471f99421b8d2a255b499fba8
votdev <votdev@localhost>
2009-03-26 22:20:55 +0000

    Replace msntp with FreeBSD's ntpdate.

--- www/license.php
+++ www/license.php
@@ -1,7 +1,7 @@
 #!/usr/local/bin/php
-<?php 
+<?php
 require("guiconfig.inc");
-$pgtitle = array(gettext("License & Credits")); 
+$pgtitle = array(gettext("License & Credits"));
 ?>
 <?php include("fbegin.inc");?>
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
@@ -11,40 +11,40 @@ $pgtitle = array(gettext("License & Credits"));
 				<?php html_titleline(gettext("License"));?>
 				<tr>
 					<td class="listt">
-            <p><strong>FreeNAS is Copyright &copy; 2005-2009 by Olivier Cochard-Labbe 
+            <p><strong>FreeNAS is Copyright &copy; 2005-2009 by Olivier Cochard-Labbe
               (<a href="mailto:olivier@freenas.org">olivier@freenas.org</a>).<br>
               All rights reserved.</strong></p>
 			  <p>FreeNAS&reg is a registered trademark of Olivier Cochard-Labbe.</p>
 		<p>FreeNAS is based on m0n0wall which is Copyright &copy; 2002-2007 by Manuel Kasper (mk@neon1.net).</p>
 	<p>FreeNAS code and documentation are released under the BSD license, under terms as follows:</p>
             <p> Redistribution and use in source and binary forms, with or without<br>
-              modification, are permitted provided that the following conditions 
+              modification, are permitted provided that the following conditions
               are met:<br>
               <br>
-              1. Redistributions of source code must retain the above copyright 
+              1. Redistributions of source code must retain the above copyright
               notice,<br>
               this list of conditions and the following disclaimer.<br>
               <br>
               2. Redistributions in binary form must reproduce the above copyright<br>
-              notice, this list of conditions and the following disclaimer in 
+              notice, this list of conditions and the following disclaimer in
               the<br>
               documentation and/or other materials provided with the distribution.<br>
               <br>
-              <strong>THIS SOFTWARE IS PROVIDED &quot;AS IS'' AND ANY EXPRESS 
+              <strong>THIS SOFTWARE IS PROVIDED &quot;AS IS'' AND ANY EXPRESS
               OR IMPLIED WARRANTIES,<br>
               INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY<br>
-              AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+              AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
               SHALL THE<br>
-              AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+              AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
               EXEMPLARY,<br>
-              OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+              OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
               OF<br>
-              SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+              SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
               BUSINESS<br>
-              INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+              INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
               IN<br>
               CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)<br>
-              ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+              ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
               OF THE<br>
               POSSIBILITY OF SUCH DAMAGE</strong>.</p>
 							</td>
@@ -59,15 +59,15 @@ $pgtitle = array(gettext("License & Credits"));
              <p>Stefan Hendricks (<a href="mailto:info@henmedia.de">info@henmedia.de</a>)<br>
               &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Added some system information and processes page</font></em><br>
              <p>Mat Murdock (<a href="mailto:mmurdock@kimballequipment.com">mmurdock@kimballequipment.com</a>)<br>
-              &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Improved the Rsync client page and code</font></em><br> 
+              &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Improved the Rsync client page and code</font></em><br>
              <p>Vyatcheslav Tyulyukov<br>
               &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Found and fixed some bugs: Software RAID display and beep</font></em><br>
               <p>Scott Zahn (<a href="mailto:scott@zahna.com">scott@zahna.com</a>)<br>
-              &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Create the buildingscript setupfreenas.sh</font></em><br> 
+              &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Create the buildingscript setupfreenas.sh</font></em><br>
               <p>Aliet Santiesteban Sifontes (<a href="mailto:aliet@tesla.cujae.edu.cu">aliet@tesla.cujae.edu.cu</a>)<br>
               &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Add Multilanguage support on the WebGUI</font></em><br>
               <p>Michael Mee (<a href="mailto:mm2001@pobox.com">mm2001@pobox.com</a>)<br>
-              &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Add Unison support</font></em><br>  
+              &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Add Unison support</font></em><br>
               <p>Niels Endres (<a href="mailto:niels@weaklogic.com">niels@weaklogic.com</a>)<br>
               &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Added FAT support for config file and auto detection of NIC name</font></em><br>
               <p>Dan Merschi (<a href="mailto:dan@freenaskb.info">dan@freenaskb.info</a>)<br>
@@ -99,7 +99,7 @@ $pgtitle = array(gettext("License & Credits"));
 						<p>Hiroyuki Seino (<a href="mailto:seichan-ml@wakhok.ne.jp">seichan-ml@wakhok.ne.jp</a>)<br>
               &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Japanese translator of the WebGUI</font></em><br>
 						<p>Alexander Samoilov (<a href="mailto:root@lifeslice.ru">root@lifeslice.ru</a>)<br>
-              &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Russian translator of the WebGUI</font></em><br>  
+              &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Russian translator of the WebGUI</font></em><br>
 			  		<p>Ioannis Koniaris (<a href="mailto:ikoniari@csd.auth.gr">ikoniari@csd.auth.gr</a>)<br>
               &nbsp;&nbsp;&nbsp;&nbsp;<em><font color="#666666">Greek translator of the WebGUI</font></em><br>
             <p>Daniel Nylander (<a href="mailto:po@danielnylander.se">po@danielnylander.se</a>)<br>
@@ -114,9 +114,9 @@ $pgtitle = array(gettext("License & Credits"));
             <?php html_titleline(gettext("Software used"));?>
             <tr>
             	<td class="listt">
-      <p>FreeNAS is based upon/includes various free software packages, listed 
+      <p>FreeNAS is based upon/includes various free software packages, listed
         below.<br>
-        The authors of FreeNAS would like to thank the authors of these software 
+        The authors of FreeNAS would like to thank the authors of these software
         packages for their efforts.</p>
       <p>FreeBSD (<a href="http://www.freebsd.org" target="_blank">http://www.freebsd.org</a>)<br>
         Copyright &copy; 1995-2007 The FreeBSD Project. All rights reserved.</p>
@@ -128,35 +128,31 @@ $pgtitle = array(gettext("License & Credits"));
         Copyright &copy; 2001-2007 The PHP Group. All rights reserved.</p>
       <p> Lighttpd (<a href="http://www.lighttpd.net" target="_blank">http://www.lighttpd.net</a>)<br>
         Copyright &copy; 2004 by Jan Kneschke &lt;jan@kneschke.de&gt;. All rights reserved.</p>
-      
+
       <p> OpenSSH (<a href="http://www.openssh.com" target="_blank">http://www.openssh.com</a>)<br>
         Copyright &copy; 1999-2007 OpenBSD</p>
-        
+
       <p> Samba (<a href="http://www.samba.org" target="_blank">http://www.samba.org</a>)<br>
        </p>
-       
+
       <p> Rsync (<a href="http://www.samba.org/rsync" target="_blank">http://www.samba.org/rsync</a>)<br>
        </p>
-      
+
       <p> ProFTPD - Highly configurable FTP server (<a href="http://www.proftpd.org" target="_blank">http://www.proftpd.org</a>)<br>
-        Copyright &copy; 1999, 2000-8, The ProFTPD Project</p>      
+        Copyright &copy; 1999, 2000-8, The ProFTPD Project</p>
 
 			<p>tftp-hpa (<a href="http://www.kernel.org/pub/software/network/tftp" target="_blank">http://www.kernel.org/pub/software/network/tftp</a>)<br>
 			</p>
 
       <p> Netatalk (<a href="http://netatalk.sourceforge.net" target="_blank">http://netatalk.sourceforge.net</a>)<br>
         Copyright &copy; 1990,1996 Regents of The University of Michigan</p>
-        
+
        <p> Apple Bonjour (<a href="http://developer.apple.com/networking/bonjour" target="_blank">http://developer.apple.com/networking/bonjour</a>)<br>
         Apple Public Source License.</p>
-     
+
       <p> Circular log support for FreeBSD syslogd (<a href="http://software.wwwi.com/syslogd" target="_blank">http://software.wwwi.com/syslogd</a>)<br>
         Copyright &copy; 2001 Jeff Wheelhouse (jdw@wheelhouse.org)</p>
-     
-      <p>msntp (<a href="http://www.hpcf.cam.ac.uk/export" target="_blank">http://www.hpcf.cam.ac.uk/export</a>)<br>
-        Copyright &copy; 1996, 1997, 2000 N.M. Maclaren, University of Cambridge. 
-        All rights reserved.</p>
-      
+
 	  <p>ataidle (<a href="http://www.cran.org.uk/bruce/software/ataidle.php" target="_blank">http://www.cran.org.uk/bruce/software/ataidle.php</a>)<br>
 			Copyright &copy; 2004-2005 Bruce Cran &lt;bruce@cran.org.uk&gt;. All rights reserved.</p>
 
@@ -170,7 +166,7 @@ $pgtitle = array(gettext("License & Credits"));
 	  	Copyright &copy; 2006 Alistair Crooks. All rights reserved.</p>
 
     <p>dp.SyntaxHighlighter (<a href="http://code.google.com/p/syntaxhighlighter" target="_blank">http://code.google.com/p/syntaxhighlighter</a>)<br>
-      Copyright &copy; 2004-2007 Alex Gorbatchev. All rights reserved.</p>    
+      Copyright &copy; 2004-2007 Alex Gorbatchev. All rights reserved.</p>
 
     <p>FUPPES - Free UPnP Entertainment Service (<a href="http://fuppes.ulrich-voelkel.de" target="_blank">http://fuppes.ulrich-voelkel.de</a>)<br>
       Copyright &copy; 2005 - 2007 Ulrich V&ouml;lkel (u-voelkel@users.sourceforge.net).</p>
@@ -180,7 +176,7 @@ $pgtitle = array(gettext("License & Credits"));
 
 	 <p>NTFS-3G driver (<a href="http://www.ntfs-3g.org" target="_blank">http://www.ntfs-3g.org</a>)<br>
       from Szabolcs Szakacsits.</p>
-	  
+
 	   <p>Fuse - Filesystem in Userspace (<a href="http://fuse.sourceforge.net" target="_blank">http://fuse.sourceforge.net</a>)<br>
       </p>
 		<p>e2fsprogs (<a href="http://e2fsprogs.sourceforge.net" target="_blank">http://e2fsprogs.sourceforge.net</a>)<br>
@@ -196,7 +192,7 @@ $pgtitle = array(gettext("License & Credits"));
 
 		<p>sipcalc (<a href="http://www.routemeister.net/projects/sipcalc" target="_blank">http://www.routemeister.net/projects/sipcalc</a>)<br>
       Copyright &copy; 2003 Simon Ekstrand. All rights reserved.</p>
-      
+
     <p>msmtp - An SMTP client with a sendmail compatible interface (<a href="http://msmtp.sourceforge.net" target="_blank">http://msmtp.sourceforge.net</a>)<br>
       Copyright &copy; 2008 Martin Lambers and others.</p>
 
