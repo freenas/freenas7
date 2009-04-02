2549163f31f4cfb421004d81cbdc1721cb1f2fd2
votdev <votdev@localhost>
2009-04-02 21:26:57 +0000

    Introduce new WebGUI control to manage content folder on UPnP/iTunes/DAAP service pages.

--- www/services_upnp.php
+++ www/services_upnp.php
@@ -57,20 +57,21 @@ $pconfig['profile'] = $config['upnp']['profile'];
 $pconfig['deviceip'] = $config['upnp']['deviceip'];
 $pconfig['transcoding'] = isset($config['upnp']['transcoding']);
 $pconfig['tempdir'] = $config['upnp']['tempdir'];
+$pconfig['content'] = $config['upnp']['content'];
 
-/* Set name to configured hostname if it is not set */
-if(!$pconfig['name'])
+// Set name to configured hostname if it is not set.
+if (!$pconfig['name'])
 	$pconfig['name'] = $config['system']['hostname'];
 
-if($_POST) {
+if ($_POST) {
 	unset($input_errors);
 	$pconfig = $_POST;
 
 	// Input validation.
 	if ($_POST['enable']) {
-		$reqdfields = explode(" ", "name if port home");
-		$reqdfieldsn = array(gettext("Name"), gettext("Interface"), gettext("Port"), gettext("Database directory"));
-		$reqdfieldst = explode(" ", "string string port string");
+		$reqdfields = explode(" ", "name if port content home");
+		$reqdfieldsn = array(gettext("Name"), gettext("Interface"), gettext("Port"), gettext("Content"), gettext("Database directory"));
+		$reqdfieldst = explode(" ", "string string port array string");
 
 		if ("Terratec_Noxon_iRadio" === $_POST['profile']) {
 			$reqdfields = array_merge($reqdfields, array("deviceip"));
@@ -90,13 +91,14 @@ if($_POST) {
 		// Check if port is already used.
 		if (services_is_port_used($_POST['port'], "upnp"))
 			$input_errors[] = sprintf(gettext("Port %ld is already used by another service."), $_POST['port']);
+
+		// Check port range.
+		if ($_POST['port'] && ((1024 > $_POST['port']) || (65535 < $_POST['port']))) {
+			$input_errors[] = sprintf(gettext("Invalid port! Port number must be in the range from %d to %d."), 1025, 65535);
+		}
 	}
 
-	if ($_POST['port'] && ((1024 > $_POST['port']) || (65535 < $_POST['port']))) {
-		$input_errors[] = sprintf(gettext("Invalid port! Port number must be in the range from %d to %d."), 1025, 65535);
-	}
-
-	if(!$input_errors) {
+	if (!$input_errors) {
 		$config['upnp']['enable'] = $_POST['enable'] ? true : false;
 		$config['upnp']['name'] = $_POST['name'];
 		$config['upnp']['if'] = $_POST['if'];
@@ -107,11 +109,12 @@ if($_POST) {
 		$config['upnp']['deviceip'] = $_POST['deviceip'];
 		$config['upnp']['transcoding'] = $_POST['transcoding'] ? true : false;
 		$config['upnp']['tempdir'] = $_POST['tempdir'];
+		$config['upnp']['content'] = $_POST['content'];
 
 		write_config();
 
 		$retval = 0;
-		if(!file_exists($d_sysrebootreqd_path)) {
+		if (!file_exists($d_sysrebootreqd_path)) {
 			config_lock();
 			$retval |= rc_update_service("fuppes");
 			$retval |= rc_update_service("mdnsresponder");
@@ -120,20 +123,11 @@ if($_POST) {
 
 		$savemsg = get_std_save_message($retval);
 
-		if($retval == 0) {
-			if(file_exists($d_upnpconfdirty_path))
+		if ($retval == 0) {
+			if (file_exists($d_upnpconfdirty_path))
 				unlink($d_upnpconfdirty_path);
 		}
 	}
-}
-
-if ($_GET['act'] === "del") {
-	/* Remove entry from content list */
-	$config['upnp']['content'] = array_diff($config['upnp']['content'],array($config['upnp']['content'][$_GET['id']]));
-	write_config();
-	touch($d_upnpconfdirty_path);
-	header("Location: services_upnp.php");
-	exit;
 }
 
 $a_interface = get_interface_list();
@@ -152,6 +146,12 @@ function enable_change(enable_change) {
 	document.iform.port.disabled = endis;
 	document.iform.web.disabled = endis;
 	document.iform.home.disabled = endis;
+	document.iform.content.disabled = endis;
+	document.iform.contentaddbtn.disabled = endis;
+	document.iform.contentchangebtn.disabled = endis;
+	document.iform.contentdeletebtn.disabled = endis;
+	document.iform.contentdata.disabled = endis;
+	document.iform.contentbrowsebtn.disabled = endis;
 	document.iform.profile.disabled = endis;
 	document.iform.deviceip.disabled = endis;
 	document.iform.transcoding.disabled = endis;
@@ -197,63 +197,33 @@ function transcoding_change() {
 </script>
 <form action="services_upnp.php" method="post" name="iform" id="iform">
 	<table width="100%" border="0" cellpadding="0" cellspacing="0">
-	  <tr>
-	    <td class="tabcont">
+		<tr>
+			<td class="tabcont">
 				<?php if ($input_errors) print_input_errors($input_errors); ?>
 				<?php if ($savemsg) print_info_box($savemsg); ?>
 				<?php if (file_exists($d_upnpconfdirty_path)) print_config_change_box();?>
-			  <table width="100%" border="0" cellpadding="6" cellspacing="0">
-			  	<?php html_titleline_checkbox("enable", gettext("UPnP A/V Media Server"), $pconfig['enable'] ? true : false, gettext("Enable"), "enable_change(false)");?>
+				<table width="100%" border="0" cellpadding="6" cellspacing="0">
+				<?php html_titleline_checkbox("enable", gettext("UPnP A/V Media Server"), $pconfig['enable'] ? true : false, gettext("Enable"), "enable_change(false)");?>
 					<?php html_inputbox("name", gettext("Name"), $pconfig['name'], gettext("UPnP friendly name."), true, 20);?>
 					<!--
 					<?php html_interfacecombobox("if", gettext("Interface"), $pconfig['if'], gettext("Interface to listen to."), true);?>
 					-->
-			    <tr>
-			      <td width="22%" valign="top" class="vncellreq"><?=gettext("Interface");?></td>
-			      <td width="78%" class="vtable">
-			        <select name="if" class="formfld" id="if">
-			          <?php foreach($a_interface as $if => $ifinfo):?>
-								<?php $ifinfo = get_interface_info($if); if (("up" == $ifinfo['status']) || ("associated" == $ifinfo['status'])):?>
-								<option value="<?=$if;?>"<?php if ($if == $pconfig['if']) echo "selected";?>><?=$if?></option>
-								<?php endif;?>
-			          <?php endforeach;?>
-			        </select>
-			        <br><?=gettext("Interface to listen to.");?>
-			      </td>
-			    </tr>
+				<tr>
+					<td width="22%" valign="top" class="vncellreq"><?=gettext("Interface");?></td>
+					<td width="78%" class="vtable">
+					<select name="if" class="formfld" id="if">
+						<?php foreach($a_interface as $if => $ifinfo):?>
+							<?php $ifinfo = get_interface_info($if); if (("up" == $ifinfo['status']) || ("associated" == $ifinfo['status'])):?>
+							<option value="<?=$if;?>"<?php if ($if == $pconfig['if']) echo "selected";?>><?=$if?></option>
+							<?php endif;?>
+						<?php endforeach;?>
+					</select>
+					<br><?=gettext("Interface to listen to.");?>
+					</td>
+				</tr>
 					<?php html_inputbox("port", gettext("Port"), $pconfig['port'], sprintf(gettext("Port to listen on. Only dynamic or private ports can be used (from %d through %d). Default port is %d."), 1025, 65535, 49152), true, 5);?>
 					<?php html_filechooser("home", gettext("Database directory"), $pconfig['home'], gettext("Location where the content database file will be stored."), $g['media_path'], true, 60);?>
-			    <tr>
-			      <td width="22%" valign="top" class="vncellreq"><?=gettext("Content");?></td>
-			      <td width="78%" class="vtable">
-			        <table width="100%" border="0" cellpadding="0" cellspacing="0">
-			          <tr>
-			            <td width="90%" class="listhdrr"><?=gettext("Directory");?></td>
-			            <td width="10%" class="list"></td>
-			          </tr>
-								<?php if (is_array($config['upnp']['content'])):?>
-								<?php $i = 0; foreach($config['upnp']['content'] as $contentv): ?>
-								<tr>
-									<td class="listlr"><?=htmlspecialchars($contentv);?> &nbsp;</td>
-									<td valign="middle" nowrap class="list">
-										<?php if(isset($config['upnp']['enable'])):?>
-										<a href="services_upnp_edit.php?id=<?=$i;?>"><img src="e.gif" title="<?=gettext("Edit directory");?>" border="0"></a>&nbsp;
-										<a href="services_upnp.php?act=del&id=<?=$i;?>" onclick="return confirm('<?=gettext("Do you really want to delete this directory entry?");?>')"><img src="x.gif" title="<?=gettext("Delete directory"); ?>" border="0"></a>
-										<?php endif; ?>
-									</td>
-								</tr>
-								<?php $i++; endforeach;?>
-								<?php endif;?>
-								<tr>
-									<td class="list" colspan="1"></td>
-									<td class="list">
-										<a href="services_upnp_edit.php"><img src="plus.gif" title="<?=gettext("Add directory");?>" border="0"></a>
-									</td>
-								</tr>
-							</table>
-							<?=gettext("Location of the files to share.");?>
-						</td>
-					</tr>
+					<?php html_folderbox("content", gettext("Content"), $pconfig['content'], gettext("Location of the files to share."), $g['media_path'], true);?>
 					<?php html_combobox("profile", gettext("Profile"), $pconfig['profile'], array("default" => gettext("Default"), "DLNA" => "DLNA", "PS3" => "Sony Playstation 3", "Telegent_TG100" => "Telegent TG100", "ZyXEL_DMA1000" => "ZyXEL DMA-1000", "Helios_X3000" => "Helios X3000", "DLink_DSM320" => "D-Link DSM320", "Microsoft_XBox360" => "Microsoft XBox 360", "Terratec_Noxon_iRadio" => "Terratec Noxon iRadio", "Yamaha_RXN600" => "Yamaha RX-N600", "Loewe_Connect" => "Loewe Connect"), gettext("Compliant profile to be used."), true, false, "profile_change()");?>
 					<?php html_inputbox("deviceip", gettext("Device IP"), $pconfig['deviceip'], gettext("The device's IP address."), true, 20);?>
 					<?php html_checkbox("transcoding", gettext("Transcoding"), $pconfig['transcoding'] ? true : false, gettext("Enable transcoding."), "", false, "transcoding_change()");?>
@@ -268,9 +238,9 @@ function transcoding_change() {
 					$text = "<a href='{$url}' target='_blank'>{$url}</a>";
 					?>
 					<?php html_text("url", gettext("URL"), $text);?>
-			  </table>
+				</table>
 				<div id="submit">
-					<input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save and Restart");?>" onClick="enable_change(true)">
+					<input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save and Restart");?>" onClick="onsubmit_content(); enable_change(true)">
 				</div>
 			</td>
 		</tr>
