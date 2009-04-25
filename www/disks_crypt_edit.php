5955e20ac6d0008e67fd31714860220eb1a18e24
votdev <votdev@localhost>
2009-04-25 18:27:00 +0000

    Add 'Camellia' GEOM Eli encryption algorithm.

--- www/disks_crypt_edit.php
+++ www/disks_crypt_edit.php
@@ -199,7 +199,7 @@ function ealgo_change() {
 			    </tr>
 					*/
 					?>
-					<?php $options = array("AES" => "AES", "Blowfish" => "Blowfish", "3DES" => "3DES");?>
+					<?php $options = array("AES" => "AES", "Blowfish" => "Blowfish", "Camellia" => "Camellia", "3DES" => "3DES");?>
 					<?php html_combobox("ealgo", gettext("Encryption algorithm"), $pconfig['ealgo'], $options, gettext("Encryption algorithm to use."), true, false, "ealgo_change()");?>
 					<?php $options = array("" => gettext("Default"), 128 => "128", 192 => "192", 256 => "256");?>
 					<?php html_combobox("keylen", gettext("Key length"), $pconfig['keylen'], $options, gettext("Key length to use with the given cryptographic algorithm.") . " " . gettext("The default key lengths are: 128 for AES, 128 for Blowfish and 192 for 3DES."), false);?>
