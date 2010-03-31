3300d83201d31047bf57437c1f5eac2f9199c057
daoyama <daoyama>
2010-03-31 16:00:16 +0000

    Add incomplete directory in Services|BitTorrent. Fix transmission_user = root does not work.

--- www/services_bittorrent.php
+++ www/services_bittorrent.php
@@ -58,6 +58,7 @@ $pconfig['pex'] = isset($config['bittorrent']['pex']);
 $pconfig['dht'] = isset($config['bittorrent']['dht']);
 $pconfig['encryption'] = $config['bittorrent']['encryption'];
 $pconfig['watchdir'] = $config['bittorrent']['watchdir'];
+$pconfig['incompletedir'] = $config['bittorrent']['incompletedir'];
 $pconfig['umask'] = $config['bittorrent']['umask'];
 $pconfig['extraoptions'] = $config['bittorrent']['extraoptions'];
 
@@ -120,6 +121,7 @@ if ($_POST) {
 		$config['bittorrent']['dht'] = $_POST['dht'] ? true : false;
 		$config['bittorrent']['encryption'] = $_POST['encryption'];
 		$config['bittorrent']['watchdir'] = $_POST['watchdir'];
+		$config['bittorrent']['incompletedir'] = $_POST['incompletedir'];
 		$config['bittorrent']['umask'] = $_POST['umask'];
 		$config['bittorrent']['extraoptions'] = $_POST['extraoptions'];
 
@@ -159,6 +161,8 @@ function enable_change(enable_change) {
 	document.iform.encryption.disabled = endis;
 	document.iform.watchdir.disabled = endis;
 	document.iform.watchdirbrowsebtn.disabled = endis;
+	document.iform.incompletedir.disabled = endis;
+	document.iform.incompletedirbrowsebtn.disabled = endis;
 	document.iform.umask.disabled = endis;
 	document.iform.extraoptions.disabled = endis;
 }
@@ -196,6 +200,7 @@ function authrequired_change() {
 					<?php html_inputbox("uplimit", gettext("Upload bandwidth"), $pconfig['uplimit'], gettext("The maximum upload bandwith in KB/s. An empty field means infinity."), false, 5);?>
 					<?php html_inputbox("downlimit", gettext("Download bandwidth"), $pconfig['downlimit'], gettext("The maximum download bandwith in KiB/s. An empty field means infinity."), false, 5);?>
 					<?php html_filechooser("watchdir", gettext("Watch directory"), $pconfig['watchdir'], gettext("Directory to watch for new .torrent files."), $g['media_path'], false, 60);?>
+					<?php html_filechooser("incompletedir", gettext("Incomplete directory"), $pconfig['incompletedir'], gettext("Directory to incomplete files. An empty field means disable."), $g['media_path'], false, 60);?>
 					<?php html_inputbox("umask", gettext("User mask"), $pconfig['umask'], sprintf(gettext("Use this option to override the default permission modes for newly created files (%s by default)."), "0002"), false, 3);?>
 					<?php html_inputbox("extraoptions", gettext("Extra options"), $pconfig['extraoptions'], gettext("Extra options (usually empty).") . " " . sprintf(gettext("Please check the <a href='%s' target='_blank'>documentation</a>."), "http://trac.transmissionbt.com/wiki/man/transmission-remote"), false, 40);?>
 					<?php html_separator();?>
