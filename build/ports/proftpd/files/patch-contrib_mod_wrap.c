05bdd1a4933a5261404a6e7a71fb7112e1aecd5d
zoon01 <zoon01>
2010-10-11 18:38:02 +0000

    Upgrade proftpd to 1.3.3b.

--- build/ports/proftpd/files/patch-contrib_mod_wrap.c
+++ build/ports/proftpd/files/patch-contrib_mod_wrap.c
@@ -1,13 +1,13 @@
---- contrib/mod_wrap.c.orig	2004-10-31 01:16:41.000000000 +0200
-+++ contrib/mod_wrap.c	2006-05-10 22:05:53.000000000 +0200
-@@ -888,8 +888,7 @@
+--- contrib/mod_wrap.c.orig	2009-12-10 18:59:14.000000000 +0100
++++ contrib/mod_wrap.c	2010-10-10 20:47:09.000000000 +0200
+@@ -922,8 +922,8 @@
  
    fromhost(&request);
  
 -  if (STR_EQ(eval_hostname(request.client), paranoid) ||
 -      !hosts_access(&request)) {
-+  if (!hosts_access(&request)) {
++
++      (!hosts_access(&request)) {
      char *denymsg = NULL;
  
      /* log the denied connection */
-
