3f7004cf82fb93dd141a0c1ba431130ce4caead4
zoon01 <zoon01>
2011-04-03 01:35:59 +0000

    repair transmission patchfile.

--- build/ports/transmission/files/patch-libtransmission_torrent.c
+++ build/ports/transmission/files/patch-libtransmission_torrent.c
@@ -1,10 +1,11 @@
---- libtransmission/torrent.c.orig	2011-02-27 15:39:55.000000000 -0600
-+++ libtransmission/torrent.c	2011-02-27 15:40:35.000000000 -0600
-@@ -27,6 +27,7 @@
+--- libtransmission/torrent.c.orig	2011-03-05 03:02:16.000000000 +0100
++++ libtransmission/torrent.c	2011-04-03 03:27:15.000000000 +0200
+@@ -27,7 +27,7 @@
  #include <stdarg.h>
  #include <string.h> /* memcmp */
  #include <stdlib.h> /* qsort */
+-
 +#include <signal.h> /* signal() */
- 
  #include <event2/util.h> /* evutil_vsnprintf() */
  
+ #include "transmission.h"
